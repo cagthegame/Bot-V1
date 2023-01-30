f3a32c82ec9501135650c5c3625a90b7b9c65151
Matt Meléndez <tslrame369@gmail.com>
2023-01-30 12:16:54 +0000

    @NeKosmic ✍️🌌

--- test.js
+++ test.js
@@ -1,30 +1,11 @@
-import fs from 'fs'
-import path, { dirname } from 'path'
-import assert from 'assert'
-import { spawn } from 'child_process'
-import syntaxError from 'syntax-error'
-import { fileURLToPath } from 'url'
-import { createRequire } from 'module'
-import chalk from 'chalk'
+/**
+</> Original base BochilGaming 
+</> Recode simple by @NeKosmic
+**/
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-const require = createRequire(__dirname)
+import e from"express";import t from"path";import{createServer as n}from"http";import{Server as i}from"socket.io";import{toBuffer as o}from"qrcode";import p from"node-fetch";import r from"./lib/helper.js";function connect(p,c){let m=global.app=e(),a=global.server=n(m),l="invalid";p.ev.on("connection.update",function e({qr:t}){t&&(l=t)}),m.use(async(e,t)=>{t.setHeader("content-type","image/png"),t.end(await o(l))}),m.use(e.static(t.join(r.__dirname(import.meta.url),"views")));new i(a).on("connection",e=>{let{unpipeEmit:t}=pipeEmit(p,e,"conn-");e.once("disconnect",t)}),a.listen(c,()=>{console.log("Aplicaci\xf3n escuchada en el puerto: ",c),opts.keepalive&&keepAlive()})}function pipeEmit(e,t,n=""){let i=e.emit;return e.emit=function(e,...o){i.emit(e,...o),t.emit(n+e,...o)},{unpipeEmit(){e.emit=i}}}function keepAlive(){let e=`https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`;/(\/\/|\.)undefined\./.test(e)||setInterval(()=>{p(e).catch(console.error)},3e5)}export default connect;
 
-let folders = ['.', ...Object.keys(require(path.join(__dirname, './package.json')).directories)]
-let files = []
-for (let folder of folders)
-    for (let file of fs.readdirSync(folder).filter(v => v.endsWith('.js')))
-        files.push(path.resolve(path.join(folder, file)))
-for (let file of files) {
-    if (file == __filename) continue
-    console.error(chalk.red('[!] Analizando', file))
-    const error = syntaxError(fs.readFileSync(file, 'utf8'), file, {
-        sourceType: 'module',
-        allowReturnOutsideFunction: true,
-        allowAwaitOutsideFunction: true
-    })
-    if (error) assert.ok(error.length < 1, file + '\n\n' + error)
-    assert.ok(file)
-    console.log(chalk.green('[✓] Hecho', file, '\n'))
-}
+/**
+[_>] https://github.com/NeKosmic/
+[_>] https://gitlab.com/NeKosmic/
+**/
