0ddc99d7939874bad79b8e19c8d401ad9cc8c095
Matt Meléndez <tslrame369@gmail.com>
2023-01-30 12:21:28 +0000

    @NeKosmic ✍️🌌

--- server.js
+++ server.js
@@ -1,58 +1,11 @@
-import express from 'express'
-import path from 'path'
-import { createServer } from 'http'
-import { Server } from 'socket.io'
-import { toBuffer } from 'qrcode'
-import fetch from 'node-fetch'
-import Helper from './lib/helper.js'
+/**
+</> Original base BochilGaming 
+</> Recode simple by @NeKosmic
+**/
 
-function connect(conn, PORT) {
-    let app = global.app = express()
-    let server = global.server = createServer(app)
-    let _qr = 'invalid'
-    
-    conn.ev.on('connection.update', function appQR({ qr }) {
-        if (qr) _qr = qr
-    })
-    
-    app.use(async (req, res) => {
-        res.setHeader('content-type', 'image/png')
-        res.end(await toBuffer(_qr))
-    })
-    app.use(express.static(path.join(Helper.__dirname(import.meta.url), 'views')))
-    
-    let io = new Server(server)
-    io.on('connection', socket => {
-        let { unpipeEmit } = pipeEmit(conn, socket, 'conn-')
-        socket.once('disconnect', unpipeEmit)
-    })
+import e from"express";import t from"path";import{createServer as n}from"http";import{Server as i}from"socket.io";import{toBuffer as o}from"qrcode";import p from"node-fetch";import r from"./lib/helper.js";function connect(p,c){let m=global.app=e(),a=global.server=n(m),l="invalid";p.ev.on("connection.update",function e({qr:t}){t&&(l=t)}),m.use(async(e,t)=>{t.setHeader("content-type","image/png"),t.end(await o(l))}),m.use(e.static(t.join(r.__dirname(import.meta.url),"views")));new i(a).on("connection",e=>{let{unpipeEmit:t}=pipeEmit(p,e,"conn-");e.once("disconnect",t)}),a.listen(c,()=>{console.log("Aplicaci\xf3n escuchada en el puerto: ",c),opts.keepalive&&keepAlive()})}function pipeEmit(e,t,n=""){let i=e.emit;return e.emit=function(e,...o){i.emit(e,...o),t.emit(n+e,...o)},{unpipeEmit(){e.emit=i}}}function keepAlive(){let e=`https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`;/(\/\/|\.)undefined\./.test(e)||setInterval(()=>{p(e).catch(console.error)},3e5)}export default connect;
 
-    server.listen(PORT, () => {
-        console.log('Aplicación escuchada en el puerto: ', PORT)
-        if (opts['keepalive']) keepAlive()
-    })
-}
-
-function pipeEmit(event, event2, prefix = '') {
-    let old = event.emit
-    event.emit = function (event, ...args) {
-        old.emit(event, ...args)
-        event2.emit(prefix + event, ...args)
-    }
-    return {
-        unpipeEmit() {
-            event.emit = old
-        }
-    }
-}
-
-function keepAlive() {
-    const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
-    if (/(\/\/|\.)undefined\./.test(url)) return
-    setInterval(() => {
-        fetch(url).catch(console.error)
-    }, 5 * 1000 * 60)
-}
-
-
-export default connect
+/**
+[_>] https://github.com/NeKosmic/
+[_>] https://gitlab.com/NeKosmic/
+**/
