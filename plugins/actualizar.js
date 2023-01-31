dc0e878c56bc8f428c526af3e62b9d5cecd0ada4
Matt Meléndez <tslrame369@gmail.com>
2023-01-31 00:57:39 +0000

    @NeKosmic 🙀

--- plugins/actualizar.js
+++ plugins/actualizar.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import{execSync as r}from"child_process";let handler=async a=>{var t=r("git remote set-url origin https://github.com/NeKosmic/NK-BOT-MD.git && git pull");a.reply(t.toString())};handler.help=["actualizarbot"],handler.tags=["propietario"],handler.command=/^(actualizarbot|actualizar|update)$/i,handler.rowner=!0;export default handler;
+import{execSync as a}from"child_process";let handler=async r=>{var t=a("git remote set-url origin https://gitlab.com/NeKosmic/nk-bot-md.git && git pull");r.reply(t.toString())};handler.help=["actualizarbot"],handler.tags=["propietario"],handler.command=/^(actualizarbot|actualizar|update)$/i,handler.rowner=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
