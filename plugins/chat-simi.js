aff19ad426f5cb8df86023dcedf6e9dfaf1e7d38
Matt Meléndez <tslrame369@gmail.com>
2023-02-07 17:04:03 +0000

    @NeKosmic 🙀

--- plugins/chat-simi.js
+++ plugins/chat-simi.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import e from"node-fetch";let handler=async(e,{text:a,command:t})=>{if(!a)return e.reply("Y el texto ...?");try{let i=await fetchJson(`https://latam-api.vercel.app/api/simisimi?apikey=${MyApiKey}&idioma=${MultiNK.Lengua()}&chat=${encodeURIComponent(a)}`);e.reply(i.respuesta?i.respuesta:"-")}catch(r){e.reply("No hay sistema!")}};handler.help=["simi"].map(e=>e+" <texto>"),handler.tags=[""],handler.command=/^(simi|bot)$/i;export default handler;
+import e from"../lib/database.js";let handler=async(a,{text:t,command:r,conn:l})=>{let s=await l.getName(a.sender),i="",h="No hay sistema!",n=a.reply(MultiNK.Proces(s));if(e.data.chats[a.chat].soloAdmins){if(t.length<15)return a.reply(`Por favor, sea mas espec\xedfico en su mensaje`);await n;try{let o=await chatBotAI(a.chat,t,i);a.reply(o)}catch(c){a.reply(h)}}else{if(!t)return a.reply("Y el texto ...?");try{let m=await chatBotAI(a.chat,t,i);a.reply(m)}catch(p){a.reply(h)}}};handler.help=["simi"].map(e=>e+" <texto>"),handler.tags=[""],handler.command=/^(simi|bot)$/i;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
