cd789dfaa9f828b8c250b6eb1c76778f496b17cd
Matt Meléndez <tslrame369@gmail.com>
2023-02-07 17:30:34 +0000

    @NeKosmic 🙀

--- plugins/chat-simi.js
+++ plugins/chat-simi.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import e from"../lib/database.js";let handler=async(a,{text:t,command:r,conn:l})=>{let s=await l.getName(a.sender),i="sk-XRL8VTi1CYcuuVbabZsofT3BlbkFJ9YSDTGK0VMnssFw5xFtx",h="No hay sistema!",n=a.reply(MultiNK.Proces(s));if(e.data.chats[a.chat].soloAdmins){if(t.length<15)return a.reply(`Por favor, sea mas espec\xedfico en su mensaje`);await n;try{let o=await chatBotAI(a.chat,t,i);a.reply(o)}catch(c){a.reply(h)}}else{if(!t)return a.reply("Y el texto ...?");try{let m=await chatBotAI(a.chat,t,i);a.reply(m)}catch(p){a.reply(h)}}};handler.help=["simi"].map(e=>e+" <texto>"),handler.tags=[""],handler.command=/^(simi|bot)$/i;export default handler;
+import a from"../lib/database.js";let handler=async(e,{text:t,command:r,conn:l})=>{let s=await l.getName(e.sender),i="sk-XRL8VTi1CYcuuVbabZsofT3BlbkFJ9YSDTGK0VMnssFw5xFtx",c="No hay sistema!";if(a.data.chats[e.chat].soloAdmins){if(t.length<15)return e.reply(`Por favor, sea mas espec\xedfico en su mensaje`);try{e.reply(MultiNK.Proces(s));await e.reply(await chatBotAI(e.chat,t,i))}catch(h){e.reply(c)}}else{if(!t)return e.reply("Y el texto ...?");try{let n=await chatBotAI(e.chat,t,i);e.reply(n)}catch(o){e.reply(c)}}};handler.help=["simi"].map(a=>a+" <texto>"),handler.tags=[""],handler.command=/^(simi|bot|aichat)$/i;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
