fecc9927e035a79302b35cd30493d73c066166d9
Matt Meléndez <tslrame369@gmail.com>
2023-02-11 22:27:37 +0000

    @NeKosmic 🙀

--- plugins/wikipedia.js
+++ plugins/wikipedia.js
@@ -3,10 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(a,{conn:e,text:i,args:t})=>{if(!t[0])return a.reply(`Cual es su b\xfasqueda en Wikipedia?`);let r=await e.getName(a.sender),d=encodeURIComponent(i),l=a.reply(MultiNK.Bsqd(r));await l;try{let n=await fetchJson(`https://latam-api.vercel.app/api/wikipedia?apikey=${MyApiKey}&q=${d}`);if(!n.datos.wikinfo)return a.reply("[ ! ] Sin resultados");e.sendMessage(a.chat,{image:{url:n.datos.miniatura},caption:`|| *WIKIPEDIA* ||
-_~> Resultados para : ${i}_
-${"‎".repeat(850)}
-${n.datos.wikinfo}`},{quoted:a})}catch(p){a.reply(MultiNK.Error0())}};handler.help=["wikipedia"].map(a=>a+" <busqueda>"),handler.tags=["busqueda"],handler.command=/^(wikipedia)$/i,handler.limit=!0;export default handler;
+let handler=async(a,{conn:e,text:i,args:t})=>{if(!t[0])return a.reply(`Cual es su b\xfasqueda en Wikipedia?`);let r=await e.getName(a.sender),d=encodeURIComponent(i),l=a.reply(MultiNK.Bsqd(r));await l;try{let n=await fetchJson(`https://latam-api.vercel.app/api/wikipedia?apikey=${MyApiKey}&q=${d}`);if(!n.datos.wikinfo)return a.reply("[ ! ] Sin resultados");e.sendMessage(a.chat,{image:{url:n.datos.miniatura},caption:`${"|| *WIKIPEDIA* ||\n_~> Resultados para : "+i+"_"}${"\n"+"‎".repeat(850)}${"\n"+n.datos.wikinfo}`},{quoted:a})}catch(s){console.log(s),a.reply(MultiNK.Error0())}};handler.help=["wikipedia <busqueda>"],handler.tags=["busqueda"],handler.command=/^wikipedia$/i,handler.limit=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
