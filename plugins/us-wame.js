df259bd96f3e72d322bfacb38b087b60aae5909c
Matt Meléndez <tslrame369@gmail.com>
2023-02-14 01:13:35 +0000

    @NeKosmic 🙀

--- plugins/us-wame.js
+++ plugins/us-wame.js
@@ -3,17 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import*as e from"fs";let handler=async(a,{conn:t,args:r})=>{await t.profilePictureUrl(a.chat,"image").catch(e=>null);let n=await t.getName(a.sender);try{var l=await t.profilePictureUrl(a.sender,"image")}catch{var l="https://i.ibb.co/jhQ7gL0/Sin-Perfil-F.jpg"}try{var i=await global.getBuffer(l)}catch{var i=e.readFileSync("./multimedia/imagenes/avatar_contact.png")}t.relayMessage(a.chat,{extendedTextMessage:{text:`⚡ *LINKS DE TU NUMERO* ⚡
-
-*Solicitado por* : ${n} 
-
-▪︎ *Su link directo de whatsapp es* :
-
-▪︎ https://wa.me/${a.sender.split("@")[0]}
-
-*O puede usar este otro*
-
-▪︎ https://api.whatsapp.com/send?phone=${a.sender.split("@")[0]} `,contextInfo:{externalAdReply:{title:"Toque aqui para ver una prueba del link",body:NombreDelBot,previewType:"PHOTO",thumbnailUrl:"",thumbnail:i,sourceUrl:`https://wa.me/${a.sender.split("@")[0]}?text=Hola+${n}%0D%0A%E1%B4%AE%CA%B8+%E1%B4%BA%E1%B4%B7`}}}},{})};handler.help=["wame"],handler.tags=["casual"],handler.command=/^(wame|wa.me)$/i;export default handler;
+import fs from"fs";let handler=async(e,{conn:a,args:t})=>{const n=await a.profilePictureUrl(e.sender,"image").catch((e=>{})),r=await a.getName(e.sender);a.relayMessage(e.chat,{extendedTextMessage:{text:`⚡ *LINKS DE TU NUMERO* ⚡\n\n*Solicitado por* : ${r} \n\n▪︎ *Su link directo de whatsapp es* :\n\n▪︎ https://wa.me/${e.sender.split("@")[0]}\n\n*O puede usar este otro*\n\n▪︎ https://api.whatsapp.com/send?phone=${e.sender.split("@")[0]} `,contextInfo:{externalAdReply:{title:"Toque aqui para ver una prueba del link",body:NombreDelBot,previewType:"PHOTO",thumbnailUrl:n,thumbnail:fs.readFileSync("./multimedia/imagenes/avatar_contact.png"),sourceUrl:`https://wa.me/${e.sender.split("@")[0]}?text=Hola+${r}%0D%0A%E1%B4%AE%CA%B8+%E1%B4%BA%E1%B4%B7`}}}},{})};handler.help=["wame"],handler.tags=["casual"],handler.command=/^(wame|wa.me)$/i;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
