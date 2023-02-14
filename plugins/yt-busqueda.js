c9247c7b668b3c218de509e39dbbaf8d6681ba99
Matt Meléndez <tslrame369@gmail.com>
2023-02-14 00:56:07 +0000

    @NeKosmic 🙀

--- plugins/yt-busqueda.js
+++ plugins/yt-busqueda.js
@@ -3,24 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(a,{conn:e,text:n,command:t})=>{if(!n)return a.reply(`Que desea buscar en Youtube?, Ejemplo de uso: 
-
-${Prefijo+t} cuanto es 1 + 1`);let r;await a.reply(MultiNK.Bsqd(await e.getName(a.sender)));let l=encodeURIComponent(n);try{let s=await fetchJson(`https://latam-api.vercel.app/api/yts?apikey=${MyApiKey}&q=${l}`),i="*[ > ] Resultados en Youtube para:* _"+n+"_\n\n";for(let u of s.resultados)i+=`🔖 Titulo: ${u.titulo}
-🧬 ID: ${u.id}
-⛓️ URL: ${u.url}
-🗜️ Tipo: ${u.tipo}
-🖼️ Miniatura: ${u.imagen}
-⌚ Duracion: ${u.duracion}
-📜 Descripci\xf3n: ${u.descripcion}
-📆 Fecha de subida: ${u.f_carga}
-👀 Vistas: ${u.vistas}
-||
-⚡Autor: ${u.autor}
-📺 Canal: ${u.canal}
-
-*——————————*
-
-`;a.reply(i)}catch(o){a.reply(MultiNK.Error0())}};handler.help=["ytbuscar <texto>"],handler.tags=["busqueda"],handler.command=/^(ytbuscar|ytsearch|yts)$/i;export default handler;
+export default handler;let handler=async(a,{conn:e,text:n,command:t})=>{if(!n)return a.reply(`Que desea buscar en Youtube?, Ejemplo de uso: \n\n${Prefijo+t} cuanto es 1 + 1`);const r=await e.getName(a.sender),o=a.reply(MultiNK.Bsqd(r));await o;const s=encodeURIComponent(n);try{const e=await fetchJson(`https://latam-api.vercel.app/api/yts?apikey=${MyApiKey}&q=${s}`);let t="*[ > ] Resultados en Youtube para:* _"+n+"_\n\n";for(let a of e.resultados)t+=`🔖 Titulo: ${a.titulo}\n🧬 ID: ${a.id}\n⛓️ URL: ${a.url}\n🗜️ Tipo: ${a.tipo}\n🖼️ Miniatura: ${a.imagen}\n⌚ Duracion: ${a.duracion}\n📜 Descripción: ${a.descripcion}\n📆 Fecha de subida: ${a.f_carga}\n👀 Vistas: ${a.vistas}\n||\n⚡Autor: ${a.autor}\n📺 Canal: ${a.canal}\n\n*——————————*\n\n`;a.reply(t)}catch(e){a.reply(MultiNK.Error0())}};handler.help=["ytbuscar <texto>"],handler.tags=["busqueda"],handler.command=/^(ytbuscar|ytsearch|yts)$/i;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
