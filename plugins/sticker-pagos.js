41b4719e8ca9197d54700316969290ee494177fd
Matt Meléndez <tslrame369@gmail.com>
2023-02-17 21:19:12 +0000

    @NeKosmic 🙀

--- plugins/sticker-pagos.js
+++ plugins/sticker-pagos.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(e,{conn:s,args:a})=>{if(!e.quoted)return e.reply("[ ! ] Responda un sticker!");/sticker/.test(e.quoted.mtype)&&(await s.relayMessage(e.chat,{requestPaymentMessage:{currencyCodeIso4217:"USD",amount1000:priceRand(10,1e4),noteMessage:{stickerMessage:Object.assign({},e.message.extendedTextMessage.contextInfo.quotedMessage.stickerMessage)}}},{}),reacMoji(e.chat,s,"⚙️",e))};handler.help=["spago"],handler.tags=["conversor"],handler.command=/^(spago|sprecio|scosto)$/i;export default handler;function priceRand(e,s){return Math.floor(Math.random()*(s-e+1)+e)}
+let handler=async(e,{conn:a,args:s})=>{if(!e.quoted)return e.reply("[ ! ] Responda un sticker!");const t=pickRandom([[10,1e4],[1,1e3],[10,1e4],[100,1e5]]);/sticker/.test(e.quoted.mtype)&&(await a.relayMessage(e.chat,{requestPaymentMessage:{currencyCodeIso4217:"USD",amount1000:priceRand(t[0],t[1]),noteMessage:{stickerMessage:Object.assign({},e.message.extendedTextMessage.contextInfo.quotedMessage.stickerMessage)}}},{}),reacMoji(e.chat,a,"⚙️",e))};handler.help=["spago"],handler.tags=["conversor"],handler.command=/^(spago|sprecio|scosto)$/i;export default handler;function priceRand(e,a){return Math.floor(Math.random()*(a-e+1)+e)}
 
 /**
 [_>] https://github.com/NeKosmic/
