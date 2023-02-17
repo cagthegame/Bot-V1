90a8b601157e891f42f89cd6d0305464d06ace0a
Matt Meléndez <tslrame369@gmail.com>
2023-02-17 20:49:11 +0000

    @NeKosmic 🙀

--- plugins/us-ascender.js
+++ plugins/us-ascender.js
@@ -3,10 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(a,{conn:e,text:r,command:d})=>r?r.match(/@/g)?void(await e.groupParticipantsUpdate(a.chat,[r.replace(/[^0-9]/g,"")+"@s.whatsapp.net"],"promote").catch(a=>{console.log(a)}),reacMoji(a.chat,e,"⚔️",a)):a.reply(`Ejemplo de uso:
-
-${Prefijo+d} @+51995...
-`):a.reply("*A que participante desea convertirlo en administrador?*");handler.help=["darpoder @usuario"],handler.tags=["grupos","admins"],handler.command=/^(darpoder|ascender|promote|daradmin)$/i,handler.group=!0,handler.admin=!0,handler.botAdmin=!0;export default handler;
+let handler=async(a,{conn:e,text:r,command:n})=>{if(!r)return a.reply("*A que participante desea convertirlo en administrador?*");if(!r.match(/@/g))return a.reply(`Ejemplo de uso:\n\n${Prefijo+n} @+51995...\n`);const d=r.replace(/[^0-9]/g,"")+"@s.whatsapp.net";await e.groupParticipantsUpdate(a.chat,[d],"promote").catch((a=>{console.log(a)})),reacMoji(a.chat,e,"⚔️",a)};handler.help=["darpoder @usuario"],handler.tags=["grupos","admins"],handler.command=/^(darpoder|ascender|promote|daradmin)$/i,handler.group=!0,handler.admin=!0,handler.botAdmin=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
