05bd23b276e1dc774bd337eb0b2483af12585aac
Matt Meléndez <tslrame369@gmail.com>
2023-02-17 21:53:45 +0000

    @NeKosmic 🙀

--- plugins/rpg-mensual.js
+++ plugins/rpg-mensual.js
@@ -3,8 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import e from"../lib/database.js";let rewards={exp:5e4,nkoins:49999,elixir:10,mitico:3,legendario:1},cooldown=2592e6,handler=async a=>{let l=e.data.users[a.sender];if(new Date-l.lastmonthly<2592e6)return a.reply(`Ya has reclamado tu regalo mensual, siguiente reclamo en *${clockString(l.lastmonthly+2592e6-new Date)}.*`);let n="";for(let r of Object.keys(rewards))r in l&&(l[r]+=rewards[r],n+=`*+${rewards[r]}* ${rpg.emoticon(r)}${r}
-`);a.reply(n),l.lastmonthly=new Date*1};handler.help=["mensual"],handler.tags=["games"],handler.command=/^(mensual)$/i,handler.cooldown=2592e6;export default handler;
+import db from"../lib/database.js";const rewards={exp:5e4,nkoins:49999,elixir:10,mitico:3,legendario:1},cooldown=2592e6;let handler=async e=>{const a=db.data.users[e.sender];if(new Date-a.lastmonthly<cooldown)return e.reply(`Ya has reclamado tu regalo mensual, siguiente reclamo en *${clockString(a.lastmonthly+cooldown-new Date)}.*`);let n="";for(let e of Object.keys(rewards))e in a&&(a[e]+=rewards[e],n+=`*+${rewards[e]}* ${rpg.emoticon(e)}${e}\n`);e.reply(n),a.lastmonthly=1*new Date};handler.help=["mensual"],handler.tags=["games"],handler.command=/^mensual$/i,handler.cooldown=cooldown;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
