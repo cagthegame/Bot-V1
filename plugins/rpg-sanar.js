d51c40743ba84e8258b57f1b1285e3aef32e6aa7
Matt Meléndez <tslrame369@gmail.com>
2023-02-17 21:52:34 +0000

    @NeKosmic 🙀

--- plugins/rpg-sanar.js
+++ plugins/rpg-sanar.js
@@ -3,16 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import e from"../lib/database.js";let handler=async(r,{args:i})=>{if(!i[0])return r.reply("Proporcione una cantidad...");let a=e.data.users[r.sender];if(a.salud>=100)return r.reply(`
-Tu ❤️salud esta lleno
-`.trim());let n=40+4*a.neko,l=1*Math.max(1,Math.min(Number.MAX_SAFE_INTEGER,isNumber(i[0])&&parseInt(i[0])||Math.round((100-a.salud)/n)));if(a.elixir<l)return r.reply(`
-Tu 🥤elixir no es suficiente, tienes *${a.elixir}*
-use el comando *
-
-${Prefijo}comprar elixir ${l-a.elixir}
-`.trim());a.elixir-=1*l,a.salud+=n*l,r.reply(`
-Obtienes *${l}* 🥤elixir(s)
-`.trim())};handler.help=["sanar"],handler.tags=["games"],handler.command=/^(sanar)$/i;export default handler;function isNumber(e){return e?"number"==typeof(e=parseInt(e))&&!isNaN(e):e}
+import db from"../lib/database.js";let handler=async(e,{args:r})=>{if(!r[0])return e.reply("Proporcione una cantidad...");const n=db.data.users[e.sender];if(n.salud>=100)return e.reply("\nTu ❤️salud esta lleno\n".trim());const a=40+4*n.neko;let i=1*Math.max(1,Math.min(Number.MAX_SAFE_INTEGER,isNumber(r[0])&&parseInt(r[0])||Math.round((100-n.salud)/a)));if(n.elixir<i)return e.reply(`\nTu 🥤elixir no es suficiente, tienes *${n.elixir}*\nuse el comando *\n\n${Prefijo}comprar elixir ${i-n.elixir}\n`.trim());n.elixir-=1*i,n.salud+=a*i,e.reply(`\nObtienes *${i}* 🥤elixir(s)\n`.trim())};handler.help=["sanar"],handler.tags=["games"],handler.command=/^sanar$/i;export default handler;function isNumber(e){return e?"number"==typeof(e=parseInt(e))&&!isNaN(e):e}
 
 /**
 [_>] https://github.com/NeKosmic/
