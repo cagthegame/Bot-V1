cf4de8f44ec6c9b1f498845c4ff6e380c52c53a1
Matt Meléndez <tslrame369@gmail.com>
2023-02-17 20:46:50 +0000

    @NeKosmic 🙀

--- plugins/us-degradar.js
+++ plugins/us-degradar.js
@@ -3,10 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(a,{conn:e,text:r,command:d})=>r?r.match(/@/g)?void(await e.groupParticipantsUpdate(a.chat,[r.replace(/[^0-9]/g,"")+"@s.whatsapp.net"],"demote").catch(a=>{console.log(a)}),reacMoji(a.chat,e,"⚔️",a)):a.reply(`Ejemplo de uso:
-
-${Prefijo+d} @+51995xxx
-`):a.reply("*A que administrador desea quitarle su puesto?*");handler.help=["quitarpoder @usuario"],handler.tags=["grupos","admins"],handler.command=/^(quitarpoder|degradar|demote|quitaradmin)$/i,handler.group=!0,handler.admin=!0,handler.botAdmin=!0;export default handler;
+let handler=async(a,{conn:e,text:r,command:t})=>{if(!r)return a.reply("*A que administrador desea quitarle su puesto?*");if(!r.match(/@/g))return a.reply(`Ejemplo de uso:\n\n${Prefijo+t} @+51995xxx\n`);const n=r.replace(/[^0-9]/g,"")+"@s.whatsapp.net";await e.groupParticipantsUpdate(a.chat,[n],"demote").catch((a=>{console.log(a)})),reacMoji(a.chat,e,"⚔️",a)};handler.help=["quitarpoder @usuario"],handler.tags=["grupos","admins"],handler.command=/^(quitarpoder|degradar|demote|quitaradmin)$/i,handler.group=!0,handler.admin=!0,handler.botAdmin=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
