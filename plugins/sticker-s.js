03831731880de065c5885d690f2efde85658392b
Matt Meléndez <tslrame369@gmail.com>
2023-02-17 21:05:41 +0000

    @NeKosmic 🙀

--- plugins/sticker-s.js
+++ plugins/sticker-s.js
@@ -3,35 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import{sticker as e}from"../lib/sticker.js";import t from"../lib/uploadFile.js";import o from"../lib/uploadImage.js";import{webp2png as a}from"../lib/webp2mp4.js";import i from"moment-timezone";let handler=async(r,{conn:n,args:d,text:s,command:l,groupMetadata:m})=>{let p=await n.getName(r.sender),u=i().tz(Intl.DateTimeFormat().resolvedOptions().timeZone).format("DD/MM/YY HH:mm:ss"),c=(p||"-").length>4?`🧰 ${p}
-`:"",g=(m.subject||"-").length>9?`
-⚙️ ${m.subject}
-`:"",f=s.length>0?`
-
-"${s}"`:`
-
-⌚ ${u}`,_=!1;try{let w=r.quoted?r.quoted:r,h=(w.msg||w).mimetype||w.mediaType||"";if(/webp|image|video/g.test(h)){if(/video/g.test(h)&&(w.msg||w).seconds>11)return r.reply(`*[ ! ] M\xe1xima duraci\xf3n de v\xeddeo son 10 segundos!*`);let v=await w.download?.();if(!v)return r.reply(`*[ ! ] Por favor Envie o Responda un video o una imagen usando el comando ${Prefijo+l}*
-_NOTA : duracion de video 1 a 10 segundos m\xe1ximo_ ✓`);r.reply(MultiNK.Proces(p));let y;try{_=await e(v,!1,"",`
-${c}${g}
-🤖 ${NombreDelBot}${f}`),reacMoji(r.chat,n,"⚙️",r)}catch(b){console.error(b)}finally{_||(/webp/g.test(h)?y=await a(v):/video/g.test(h)&&(y=await t(v)),y&&"string"==typeof y||(y=await o(v)),_=await e(!1,y,"",`
-${c}${g}
-🤖 ${NombreDelBot}${f}`))}}else if(d[0]){if(!isUrl(d[0]))return r.reply("[ ! ] Url inv\xe1lido, prueba con otro ;3");_=await e(!1,d[0],"",`
-${c}${g}
-🤖 ${NombreDelBot}
-
-⌚ ${u}
-
-
-
-
-
-
-
-
-
-
-`)}}catch(x){console.error(x),_||(_=x)}finally{if(!_)return r.reply(`*[ ! ] Por favor Envie o Responda un video o una imagen usando el comando ${Prefijo+l}*
-_NOTA : duracion de video 1 a 10 segundos m\xe1ximo_ ✓`);n.sendFile(r.chat,_,"sticker.webp","",r)}};handler.help=["sticker [multimedia/url]"],handler.tags=["conversor"],handler.command=/^s(tic?ker)?(gif)?$/i;export default handler;let isUrl=e=>e.match(RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/,"gi"));
+import{sticker}from"../lib/sticker.js";import uploadFile from"../lib/uploadFile.js";import uploadImage from"../lib/uploadImage.js";import{webp2png}from"../lib/webp2mp4.js";import moment from"moment-timezone";let handler=async(e,{conn:o,args:n,text:t,command:a,groupMetadata:i})=>{const r=await o.getName(e.sender),l=moment().tz(Intl.DateTimeFormat().resolvedOptions().timeZone).format("DD/MM/YY HH:mm:ss"),m=(r||"-").length>4?`🧰 ${r}\n`:"",s=(i.subject||"-").length>9?`\n⚙️ ${i.subject}\n`:"",d=t.length>0?`\n\n"${t}"`:`\n\n⌚ ${l}`,p=!1;try{let t=e.quoted?e.quoted:e,i=(t.msg||t).mimetype||t.mediaType||"";if(/webp|image|video/g.test(i)){if(/video/g.test(i)&&(t.msg||t).seconds>11)return e.reply("*[ ! ] Máxima duración de vídeo son 10 segundos!*");let n,l=await(t.download?.());if(!l)return e.reply(`*[ ! ] Por favor Envie o Responda un video o una imagen usando el comando ${Prefijo+a}*\n_NOTA : duracion de video 1 a 10 segundos máximo_ ✓`);e.reply(MultiNK.Proces(r));try{p=await sticker(l,!1,"",`\n${m}${s}\n🤖 ${NombreDelBot}${d}`),reacMoji(e.chat,o,"⚙️",e)}catch(e){console.error(e)}finally{p||(/webp/g.test(i)?n=await webp2png(l):/video/g.test(i)&&(n=await uploadFile(l)),n&&"string"==typeof n||(n=await uploadImage(l)),p=await sticker(!1,n,"",`\n${m}${s}\n🤖 ${NombreDelBot}${d}`))}}else if(n[0]){if(!isUrl(n[0]))return e.reply("[ ! ] Url inválido, prueba con otro ;3");p=await sticker(!1,n[0],"",`\n${m}${s}\n🤖 ${NombreDelBot}\n\n⌚ ${l}\n\n\n\n\n\n\n\n\n\n\n`)}}catch(e){console.error(e),p||(p=e)}finally{if(!p)return e.reply(`*[ ! ] Por favor Envie o Responda un video o una imagen usando el comando ${Prefijo+a}*\n_NOTA : duracion de video 1 a 10 segundos máximo_ ✓`);o.sendFile(e.chat,p,"sticker.webp","",e)}};handler.help=["sticker [multimedia/url]"],handler.tags=["conversor"],handler.command=/^s(tic?ker)?(gif)?$/i;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
