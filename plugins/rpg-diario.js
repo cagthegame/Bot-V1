1b6c4026ffb36184b350c6a5689ff6a8eb699f64
Matt Meléndez <tslrame369@gmail.com>
2023-02-17 21:56:43 +0000

    @NeKosmic 🙀

--- plugins/rpg-diario.js
+++ plugins/rpg-diario.js
@@ -3,9 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import e from"../lib/database.js";let rewards={exp:9999,nkoins:4999,elixir:5},cooldown=864e5,handler=async a=>{let r=e.data.users[a.sender];if(new Date-r.lastclaim<864e5)return a.reply(`\xa1Ya has reclamado tu recompensa diaria!, vuelva despu\xe9s de *${clockString(r.lastclaim+864e5-new Date)}.*`);let l="";for(let d of Object.keys(rewards))d in r&&(r[d]+=rewards[d],l+=`
-*+${rewards[d]}* ${global.rpg.emoticon(d)}${d}
-`);a.reply(l),r.lastclaim=new Date*1};handler.help=["diario"],handler.tags=["games"],handler.command=/^(diario)$/i,handler.cooldown=864e5;export default handler;
+import db from"../lib/database.js";const rewards={exp:9999,nkoins:4999,elixir:5},cooldown=864e5;let handler=async e=>{const a=db.data.users[e.sender];if(new Date-a.lastclaim<864e5)return e.reply(`¡Ya has reclamado tu recompensa diaria!, vuelva después de *${clockString(a.lastclaim+864e5-new Date)}.*`);let r="";for(let e of Object.keys(rewards))e in a&&(a[e]+=rewards[e],r+=`\n*+${rewards[e]}* ${global.rpg.emoticon(e)}${e}\n`);e.reply(r),a.lastclaim=1*new Date};handler.help=["diario"],handler.tags=["games"],handler.command=/^diario$/i,handler.cooldown=864e5;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
