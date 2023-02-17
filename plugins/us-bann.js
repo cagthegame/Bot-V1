d101758efef0cdddaf8b0998fc18f05596714661
Matt Meléndez <tslrame369@gmail.com>
2023-02-17 20:48:05 +0000

    @NeKosmic 🙀

--- plugins/us-bann.js
+++ plugins/us-bann.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import a from"../lib/database.js";let handler=async(e,{conn:r,text:n})=>{if(!n)return e.reply("A quien desea banear?");let o;if(!(o=e.isGroup?e.mentionedJid[0]:e.chat))return e.reply("[ ! ] Por favor etiquete al usuario que desea banear");a.data.users[o].banned=!0,r.reply(e.chat,`Usuario baneado, ${o} ya no podra usar al bot [ ! ]`,e)};handler.help=["banear @usuario"],handler.tags=["propietario"],handler.command=/^banear$/i,handler.rowner=!0;export default handler;
+import db from"../lib/database.js";let handler=async(a,{conn:e,text:r})=>{if(!r)return a.reply("A quien desea banear?");let n;if(n=a.isGroup?a.mentionedJid[0]:a.chat,!n)return a.reply("[ ! ] Por favor etiquete al usuario que desea banear");db.data.users[n].banned=!0,e.reply(a.chat,`Usuario baneado, ${n} ya no podra usar al bot [ ! ]`,a)};handler.help=["banear @usuario"],handler.tags=["propietario"],handler.command=/^banear$/i,handler.rowner=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
