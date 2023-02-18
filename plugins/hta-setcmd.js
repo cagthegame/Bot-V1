fd114994bbabd535b83e82b355e260d98bd09c23
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 02:52:06 +0000

    @NeKosmic 🙀

--- plugins/hta-setcmd.js
+++ plugins/hta-setcmd.js
@@ -3,10 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import e from"../lib/database.js";let handler=async(r,{text:t,command:a})=>{if(e.data.sticker=e.data.sticker||{},!r.quoted)return r.reply(`Responda un sticker *${Prefijo+a}*`);if(!r.quoted.fileSha256)return r.reply("Falta hash SHA256(sticker)");if(!t)return r.reply(`Ejemplo de uso:
-
-${Prefijo+a} ${Prefijo}menu
-`);let d=e.data.sticker,n=r.quoted.fileSha256.toString("base64");if(d[n]&&d[n].locked)return r.reply("No tienes permiso para realizar esta solicitud.");d[n]={text:t,mentionedJid:r.mentionedJid,creator:r.sender,at:+new Date,locked:!1},r.reply(`Completado correctamente ✓`)};handler.help=["cmd"].map(e=>"set"+e+" <texto>"),handler.tags=["herramienta"],handler.command=/^setcmd$/i,handler.premium=!0;export default handler;
+import db from"../lib/database.js";let handler=async(e,{text:t,command:r})=>{if(db.data.sticker=db.data.sticker||{},!e.quoted)return e.reply(`Responda un sticker *${Prefijo+r}*`);if(!e.quoted.fileSha256)return e.reply("Falta hash SHA256(sticker)");if(!t)return e.reply(`Ejemplo de uso:\n\n${Prefijo+r} ${Prefijo}menu\n`);let a=db.data.sticker;const d=e.quoted.fileSha256.toString("base64");if(a[d]&&a[d].locked)return e.reply("No tienes permiso para realizar esta solicitud.");a[d]={text:t,mentionedJid:e.mentionedJid,creator:e.sender,at:+new Date,locked:!1},e.reply("Completado correctamente ✓")};handler.help=["setcmd <texto>"],handler.tags=["herramienta"],handler.command=/^setcmd$/i,handler.premium=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
