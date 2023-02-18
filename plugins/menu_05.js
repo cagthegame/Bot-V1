cd3cdb6f33482344f5d69f59b89a8b5a44a2d19d
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 01:06:27 +0000

    @NeKosmic 🙀

--- plugins/menu_05.js
+++ plugins/menu_05.js
@@ -3,13 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import*as a from"fs";let handler=async(e,{conn:i})=>{await i.sendMessage(e.chat,{image:{url:await i.profilePictureUrl(e.chat,"image").catch(a=>"./multimedia/imagenes/myunivers.jpg")},jpegThumbnail:a.readFileSync("./multimedia/imagenes/mylogo.jpg"),caption:`
-╔═══════════
-╟{ MENU +18 }
-║
-║㋡ ${Prefijo}hentai
-╚═══════════
-`.trim()},{quoted:e})};handler.help=["labiblia"],handler.command=/^(labiblia)$/i,handler.group=!0,handler.premium=!0;export default handler;
+import fs from"fs";let handler=async(a,{conn:e})=>{const i=await e.profilePictureUrl(a.chat,"image").catch((a=>"./multimedia/imagenes/myunivers.jpg"));await e.sendMessage(a.chat,{image:{url:i},jpegThumbnail:fs.readFileSync("./multimedia/imagenes/mylogo.jpg"),caption:`\n╔═══════════\n╟{ MENU +18 }\n║\n║㋡ ${Prefijo}hentai\n╚═══════════\n`.trim()},{quoted:a})};handler.help=["labiblia"],handler.command=/^labiblia$/i,handler.group=!0,handler.premium=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
