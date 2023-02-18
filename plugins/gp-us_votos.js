40e17836b58fc98518da0b4a6c8de6cb896311a3
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 03:25:21 +0000

    @NeKosmic 🙀

--- plugins/gp-us_votos.js
+++ plugins/gp-us_votos.js
@@ -3,23 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(t,{conn:o,command:a})=>{let e=t.chat;if(o.vote=o.vote?o.vote:{},!(e in o.vote))return t.reply(`*[ ! ] Aun no hay una votaci\xf3n activa en este grupo*
-_Para crear una nueva votacion use el comando_ ${Prefijo}votacion`);let n=o.vote[e][1].concat(o.vote[e][2]),r=n.includes(t.sender);if(r)return t.reply("*[ ! ] No puedes volver a votar*");if(/sivotar/i.test(a)){o.vote[e][1].push(t.sender);var i="\uD83D\uDC4D"}else if(/novotar/i.test(a)){o.vote[e][2].push(t.sender);var i="\uD83D\uDC4E"}let[s,v,l]=o.vote[e],p=`\`\`\`[ VOTACI\xd3N ]\`\`\`
-
-📋 *Razon:* ${s}
-✍️ *Votante:* ${"@"+t.sender.split("@")[0]}
-
-📝
-┣🫂
-┣━⊱ Votos a favor [✓]
-${v.map((t,o)=>`┣ ${o+1}. @${t.split`@`[0]}`).join("\n")?v.map((t,o)=>`┣ ${o+1}. @${t.split`@`[0]}`).join("\n"):"┣ Aun no hay :v"}
-┗━━⊱ Total: ${v.length}
-
-📝
-┣👥
-┣━⊱ Votos en contra [X]
-${l.map((t,o)=>`┣ ${o+1}. @${t.split`@`[0]}`).join("\n")?l.map((t,o)=>`┣ ${o+1}. @${t.split`@`[0]}`).join("\n"):"┣ Aun no hay :v"}
-┗━━⊱ Total: ${l.length}`.trim();await o.sendButton(t.chat,p,NombreDelBot+` 🔥`,null,[["Si Votar ✔️",`${Prefijo}sivotar`],["No Votar ❌",`${Prefijo}novotar`]],t,{mentions:o.parseMention(p)}),reacMoji(t.chat,o,i,t)};handler.help=["sivotar","novotar"],handler.tags=["grupos"],handler.command=/^(sivotar|novotar)$/i,handler.group=!0;export default handler;
+let handler=async(n,{conn:o,command:t})=>{const e=n.chat;if(o.vote=o.vote?o.vote:{},!(e in o.vote))return n.reply(`*[ ! ] Aun no hay una votación activa en este grupo*\n_Para crear una nueva votacion use el comando_ ${Prefijo}votacion`);if(o.vote[e][1].concat(o.vote[e][2]).includes(n.sender))return n.reply("*[ ! ] No puedes volver a votar*");if(/sivotar/i.test(t)){o.vote[e][1].push(n.sender);var a="👍"}else if(/novotar/i.test(t)){o.vote[e][2].push(n.sender);a="👎"}const[r,i,s]=o.vote[e],v=`\`\`\`[ VOTACIÓN ]\`\`\`\n\n📋 *Razon:* ${r}\n✍️ *Votante:* ${"@"+n.sender.split("@")[0]}\n\n📝\n┣🫂\n┣━⊱ Votos a favor [✓]\n${i.map(((n,o)=>`┣ ${o+1}. @${n.split`@`[0]}`)).join("\n")?i.map(((n,o)=>`┣ ${o+1}. @${n.split`@`[0]}`)).join("\n"):"┣ Aun no hay :v"}\n┗━━⊱ Total: ${i.length}\n\n📝\n┣👥\n┣━⊱ Votos en contra [X]\n${s.map(((n,o)=>`┣ ${o+1}. @${n.split`@`[0]}`)).join("\n")?s.map(((n,o)=>`┣ ${o+1}. @${n.split`@`[0]}`)).join("\n"):"┣ Aun no hay :v"}\n┗━━⊱ Total: ${s.length}`.trim();await o.sendButton(n.chat,v,NombreDelBot+" 🔥",null,[["Si Votar ✔️",`${Prefijo}sivotar`],["No Votar ❌",`${Prefijo}novotar`]],n,{mentions:o.parseMention(v)}),reacMoji(n.chat,o,a,n)};handler.help=["sivotar","novotar"],handler.tags=["grupos"],handler.command=/^(sivotar|novotar)$/i,handler.group=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
