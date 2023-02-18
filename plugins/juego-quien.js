02c8bb059d848330925ec0c89844e423ded463e0
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 02:07:26 +0000

    @NeKosmic 🙀

--- plugins/juego-quien.js
+++ plugins/juego-quien.js
@@ -3,13 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(e,{conn:n,text:a,participants:t,command:l})=>{if(!a)return e.reply(`*[ ! ] Invente un asunto*
-_Ejemplo:_
-
-${Prefijo+l} es gey?
-`);if(a.length<5)return e.reply("*[ ! ] El asunto es muy corto*");var r=[];t.map(async e=>{r.push(e.id.replace("c.us","s.whatsapp.net"))});let s=r[Math.floor(Math.random()*r.length)];await n.sendMessage(e.chat,{text:`
-👉 @${s.split("@")[0]}
-`,mentions:[s]},{ephemeralExpiration:86400,quoted:e}),reacMoji(e.chat,n,"\uD83E\uDD39‍♂️",e)};handler.help=["quien"],handler.tags=["games"],handler.command=/^(quien)$/i,handler.limit=!0,handler.group=!0;export default handler;
+let handler=async(e,{conn:n,text:a,participants:t,command:r})=>{if(!a)return e.reply(`*[ ! ] Invente un asunto*\n_Ejemplo:_\n\n${Prefijo+r} es gey?\n`);if(a.length<5)return e.reply("*[ ! ] El asunto es muy corto*");var l=[];t.map((async e=>{l.push(e.id.replace("c.us","s.whatsapp.net"))}));const o=l[Math.floor(Math.random()*l.length)];await n.sendMessage(e.chat,{text:`\n👉 @${o.split("@")[0]}\n`,mentions:[o]},{ephemeralExpiration:86400,quoted:e}),reacMoji(e.chat,n,"🤹‍♂️",e)};handler.help=["quien"],handler.tags=["games"],handler.command=/^quien$/i,handler.limit=!0,handler.group=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
