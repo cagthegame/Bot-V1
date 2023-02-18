d7875293e2bab4cf8eb951e0399ebdfbef286c42
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 01:54:58 +0000

    @NeKosmic 🙀

--- plugins/limpiador.js
+++ plugins/limpiador.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import{tmpdir as r}from"os";import e,{join as a}from"path";import{readdirSync as t,statSync as o,unlinkSync as p,existsSync as l,readFileSync as m,watch as h}from"fs";let handler=async(e,{conn:l,usedPrefix:m,__dirname:h,args:d})=>{l.reply(e.chat,"Carpeta de archivos temporales fue limpiado correctamente ✓",e);let n=[r(),a(h,"../tmp")],c=[];return n.forEach(r=>t(r).forEach(e=>c.push(a(r,e)))),c.map(r=>{o(r),p(r)})};handler.help=["cleartmp"],handler.tags=["propietario"],handler.command=/^(cleartmp)$/i,handler.rowner=!0;export default handler;
+import{tmpdir}from"os";import path,{join}from"path";import{readdirSync,statSync,unlinkSync,existsSync,readFileSync,watch}from"fs";let handler=async(r,{conn:a,usedPrefix:e,__dirname:n,args:t})=>{a.reply(r.chat,"Carpeta de archivos temporales fue limpiado correctamente ✓",r);const o=[tmpdir(),join(n,"../tmp")],i=[];return o.forEach((r=>readdirSync(r).forEach((a=>i.push(join(r,a)))))),i.map((r=>{statSync(r);unlinkSync(r)}))};handler.help=["cleartmp"],handler.tags=["propietario"],handler.command=/^cleartmp$/i,handler.rowner=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
