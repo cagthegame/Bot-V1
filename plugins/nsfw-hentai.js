91da7c4f841c5142e1ff9f35061822604a5e25d3
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 00:55:51 +0000

    @NeKosmic 🙀

--- plugins/nsfw-hentai.js
+++ plugins/nsfw-hentai.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import*as e from"fs";let handler=async(a,{conn:r})=>{let t=await r.getName(a.sender),i=["\uD83E\uDD75\uD83D\uDD25","\uD83D\uDE0E\uD83D\uDC4C","\uD83D\uDE08\uD83E\uDD19","\uD83D\uDE0F","\uD83C\uDF1D\uD83E\uDD0C"],n=a.reply(MultiNK.Proces(t));await n;try{var l=await fetchJson(`https://latam-api.vercel.app/api/hentai?apikey=${MyApiKey}`);r.sendMessage(a.chat,{image:{url:l.hidrogeno},jpegThumbnail:e.readFileSync("./multimedia/imagenes/nsfw.jpg"),caption:i[Math.floor(Math.random()*i.length)]},{quoted:a})}catch(h){a.reply(MultiNK.Error0())}};handler.help=["hentai"],handler.tags=["premium"],handler.command=/^(hentai)$/i,handler.premium=!0;export default handler;
+import fs from"fs";let handler=async(e,{conn:a})=>{const t=await a.getName(e.sender),n=["🥵🔥","😎👌","😈🤙","😏","🌝🤌"],r=e.reply(MultiNK.Proces(t));await r;try{const t=await fetchJson(`https://latam-api.vercel.app/api/hentai?apikey=${MyApiKey}`);a.sendMessage(e.chat,{image:{url:t.hidrogeno},jpegThumbnail:fs.readFileSync("./multimedia/imagenes/nsfw.jpg"),caption:n[Math.floor(Math.random()*n.length)]},{quoted:e})}catch(a){e.reply(MultiNK.Error0())}};handler.help=["hentai"],handler.tags=["premium"],handler.command=/^hentai$/i,handler.premium=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
