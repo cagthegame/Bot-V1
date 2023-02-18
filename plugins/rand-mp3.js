24f219768d0f72bbca1336d5579c58e2b1f114e8
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 00:44:36 +0000

    @NeKosmic 🙀

--- plugins/rand-mp3.js
+++ plugins/rand-mp3.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import{toAudio as e}from"../lib/converter.js";import*as t from"fs";let handler=async(e,{conn:t,usedPrefix:a,command:o,text:r})=>{let d=await t.getName(e.sender),l=e.reply(MultiNK.Proces(d));await l;try{let i=await fetchJson(`https://latam-api.vercel.app/api/rand_audio?apikey=${MyApiKey}`);t.sendMessage(e.chat,{audio:{url:i.audio},contextInfo:{externalAdReply:{title:`${d} 🎧`,mediaType:2,thumbnailUrl:"https://drive.google.com/uc?export=download&id=1XtkkVvPK3bkYKsSbZotyeTNSAEoAbodq",previewType:"VIDEO",mediaUrl:"https://youtu.be/Tk9Pitk1_oM"}},fileName:"DjNK.mp3",mimetype:"audio/mpeg",ptt:!0},{quoted:e})}catch(p){e.reply(MultiNK.Error0())}};handler.help=["djbot"],handler.tags=["random"],handler.command=/^(djbot)$/i;export default handler;
+import{toAudio}from"../lib/converter.js";import fs from"fs";let handler=async(e,{conn:t,usedPrefix:a,command:o,text:r})=>{const d=await t.getName(e.sender),i=e.reply(MultiNK.Proces(d));await i;try{const a=await fetchJson(`https://latam-api.vercel.app/api/rand_audio?apikey=${MyApiKey}`);t.sendMessage(e.chat,{audio:{url:a.audio},contextInfo:{externalAdReply:{title:`${d} 🎧`,mediaType:2,thumbnailUrl:"https://drive.google.com/uc?export=download&id=1XtkkVvPK3bkYKsSbZotyeTNSAEoAbodq",previewType:"VIDEO",mediaUrl:"https://youtu.be/Tk9Pitk1_oM"}},fileName:"DjNK.mp3",mimetype:"audio/mpeg",ptt:!0},{quoted:e})}catch(t){e.reply(MultiNK.Error0())}};handler.help=["djbot"],handler.tags=["random"],handler.command=/^djbot$/i;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
