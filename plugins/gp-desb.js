a98df20e9cf93de0e775f0bd9abc5faa3d6b2025
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 03:46:56 +0000

    @NeKosmic 🙀

--- plugins/gp-desb.js
+++ plugins/gp-desb.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import a from"../lib/database.js";let handler=async e=>{a.data.chats[e.chat].isBanned=!1,e.reply("Chat desbaneado correctamente ✓")};handler.help=["unbanchat"],handler.tags=["propietario"],handler.command=/^unbanchat$/i,handler.owner=!0;export default handler;
+import db from"../lib/database.js";let handler=async a=>{db.data.chats[a.chat].isBanned=!1,a.reply("Chat desbaneado correctamente ✓")};handler.help=["unbanchat"],handler.tags=["propietario"],handler.command=/^unbanchat$/i,handler.owner=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
