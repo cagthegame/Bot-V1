8bfad5ad8af276eb724604f2e258416c532183a5
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 02:54:11 +0000

    @NeKosmic 🙀

--- plugins/hta-savefile.js
+++ plugins/hta-savefile.js
@@ -3,25 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import e from"fs";import t from"syntax-error";import o from"path";import r from"util";let _fs=e.promises,handler=async(e,{text:a,command:i,__dirname:l})=>{if(!a)throw`
-*Con que nombre desea guardar el archivo?*
-_Ejemplo de uso_ : 
-
-${Prefijo+i} test-plugin.js n`.trim();if(!e.quoted)return e.reply("[_>] Responda un mensaje que contenga una sintaxis correcta...");if(/p(lugin)?/i.test(i)){let n=a.replace(/plugin(s)\//i,"")+(/\.js$/i.test(a)?"":".js"),u=t(e.quoted.text,n,{sourceType:"module",allowReturnOutsideFunction:!0,allowAwaitOutsideFunction:!0});if(u)throw u;let s=o.join(l,n);await _fs.writeFile(s,e.quoted.text),e.reply(`
-El archivo *${n}* fue guardado correctamente ✓
-
-\`\`\`
-${r.format(e.quoted.text)}
-\`\`\`
-`.trim())}else{let d=e.quoted.text&&!e.quoted.mediaMessage&&/\.js/.test(a);if(d){let m=t(e.quoted.text,a,{sourceType:"module",allowReturnOutsideFunction:!0,allowAwaitOutsideFunction:!0});if(m)throw m;await _fs.writeFile(a,e.quoted.text),e.reply(`
-El archivo *${a}* fue guardado correctamente ✓
-
-\`\`\`
-${r.format(e.quoted.text)}
-\`\`\`
-`.trim())}else if(e.quoted.mediaMessage){let p=await e.quoted.download();await _fs.writeFile(a,p),e.reply(`
-Medios guardados con \xe9xito en *${a}*
-`.trim())}else throw"Not soportado!!!"}};handler.help=["saveplugin <nombre de archivo>"],handler.tags=["propietario","herramienta"],handler.command=/^(saveplugin)$/i,handler.rowner=!0;export default handler;
+import fs from"fs";import syntaxError from"syntax-error";import path from"path";import util from"util";const _fs=fs.promises;let handler=async(e,{text:t,command:o,__dirname:r})=>{if(!t)throw`\n*Con que nombre desea guardar el archivo?*\n_Ejemplo de uso_ : \n\n${Prefijo+o} test-plugin.js n`.trim();if(!e.quoted)return e.reply("[_>] Responda un mensaje que contenga una sintaxis correcta...");if(/p(lugin)?/i.test(o)){const o=t.replace(/plugin(s)\//i,"")+(/\.js$/i.test(t)?"":".js"),n=syntaxError(e.quoted.text,o,{sourceType:"module",allowReturnOutsideFunction:!0,allowAwaitOutsideFunction:!0});if(n)throw n;const a=path.join(r,o);await _fs.writeFile(a,e.quoted.text),e.reply(`\nEl archivo *${o}* fue guardado correctamente ✓\n\n\`\`\`\n${util.format(e.quoted.text)}\n\`\`\`\n`.trim())}else{if(e.quoted.text&&!e.quoted.mediaMessage&&/\.js/.test(t)){const o=syntaxError(e.quoted.text,t,{sourceType:"module",allowReturnOutsideFunction:!0,allowAwaitOutsideFunction:!0});if(o)throw o;await _fs.writeFile(t,e.quoted.text),e.reply(`\nEl archivo *${t}* fue guardado correctamente ✓\n\n\`\`\`\n${util.format(e.quoted.text)}\n\`\`\`\n`.trim())}else{if(!e.quoted.mediaMessage)throw"Not soportado!!!";{const o=await e.quoted.download();await _fs.writeFile(t,o),e.reply(`\nMedios guardados con éxito en *${t}*\n`.trim())}}}};handler.help=["saveplugin <nombre de archivo>"],handler.tags=["propietario","herramienta"],handler.command=/^saveplugin$/i,handler.rowner=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
