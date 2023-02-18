95bfd3d3381d7defc8453c8c3eac23af839dbd05
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 01:09:06 +0000

    @NeKosmic 🙀

--- plugins/menu_04.js
+++ plugins/menu_04.js
@@ -3,19 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import*as a from"fs";let handler=async(e,{conn:i})=>{await i.sendMessage(e.chat,{image:{url:await i.profilePictureUrl(e.chat,"image").catch(a=>"./multimedia/imagenes/myunivers.jpg")},jpegThumbnail:a.readFileSync("./multimedia/imagenes/mylogo.jpg"),caption:`
-╔═══════════
-╟{ ANIME }
-║
-║圆 ${Prefijo}anifrase
-║圆 ${Prefijo}waifu_hd
-║圆 ${Prefijo}rostro_4k
-║圆 ${Prefijo}infoanime
-║圆 ${Prefijo}neko
-║圆 ${Prefijo}snime
-║圆 ${Prefijo}waifu
-╚═══════════
-`.trim()},{quoted:e})};handler.help=["mianime"],handler.command=/^(mianime)$/i,handler.group=!0;export default handler;
+import fs from"fs";let handler=async(e,{conn:i})=>{const n=await i.profilePictureUrl(e.chat,"image").catch((e=>"./multimedia/imagenes/myunivers.jpg"));await i.sendMessage(e.chat,{image:{url:n},jpegThumbnail:fs.readFileSync("./multimedia/imagenes/mylogo.jpg"),caption:`\n╔═══════════\n╟{ ANIME }\n║\n║圆 ${Prefijo}anifrase\n║圆 ${Prefijo}waifu_hd\n║圆 ${Prefijo}rostro_4k\n║圆 ${Prefijo}infoanime\n║圆 ${Prefijo}neko\n║圆 ${Prefijo}snime\n║圆 ${Prefijo}waifu\n╚═══════════\n`.trim()},{quoted:e})};handler.help=["mianime"],handler.command=/^mianime$/i,handler.group=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
