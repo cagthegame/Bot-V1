3aafc696ea56499f95e4fda65db86113166b4c6f
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 03:45:55 +0000

    @NeKosmic 🙀

--- plugins/gp-edit_desc.js
+++ plugins/gp-edit_desc.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(e,{conn:a,args:d,text:r})=>r?r.length>=513?reply(`*[ ! ] M\xe1ximo de car\xe1cteres 512*`):void(await a.groupUpdateDescription(e.chat,r),reacMoji(e.chat,a,"✍️",e)):e.reply(`*[ ! ] Ingrese un texto con el cual se actualizar\xe1 la descripci\xf3n del grupo actual*`);handler.help=["gpdesc <texto>"],handler.tags=["grupos","admins"],handler.command=/^(gpdesc|descgp|setdesk|setdesc)$/i,handler.group=!0,handler.admin=!0,handler.botAdmin=!0;export default handler;
+let handler=async(e,{conn:a,args:d,text:r})=>r?r.length>=513?reply("*[ ! ] Máximo de carácteres 512*"):(await a.groupUpdateDescription(e.chat,r),void reacMoji(e.chat,a,"✍️",e)):e.reply("*[ ! ] Ingrese un texto con el cual se actualizará la descripción del grupo actual*");handler.help=["gpdesc <texto>"],handler.tags=["grupos","admins"],handler.command=/^(gpdesc|descgp|setdesk|setdesc)$/i,handler.group=!0,handler.admin=!0,handler.botAdmin=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
