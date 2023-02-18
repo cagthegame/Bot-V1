fff604f7e5950f80d53932f2d5e6e42d492ee3c4
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 03:37:29 +0000

    @NeKosmic 🙀

--- plugins/gp-info.js
+++ plugins/gp-info.js
@@ -3,19 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(a,{conn:e,participants:i,groupMetadata:n})=>{let t=await e.profilePictureUrl(a.chat,"image").catch(a=>null)||"./multimedia/imagenes/avatar_contact.png",r=i.filter(a=>a.admin),l=r.map((a,e)=>`${e+1}. _@${a.id.split("@")[0]}_`).join("\n"),d=n.owner||r.find(a=>"superadmin"===a.admin)?.id||a.chat.split`-`[0]+"@s.whatsapp.net",p,o=`
-*Nombre del grupo* : ${n.subject}
-*Creado por* : _${d.split("@")[0],"@"+d.split("@")[0]}_
-*Fecha de creaci\xf3n* : _${formatDate(1e3*n.creation)}_
-*Total de participantes* : _${i.length}_
-*Total de administradores* : _${i.filter(a=>"admin"===a.admin).length+i.filter(a=>"superadmin"===a.admin).length}_
-${l}
-*No administradores* : _${i.filter(a=>null===a.admin).length}_
-*ID del grupo* : _${n.id}_
-*Descripci\xf3n* : 
-${"‎".repeat(850)}
-${n.desc?.toString()}
-`.trim();e.sendFile(a.chat,t,"pp.jpg",o,a,!1,{mentions:[...r.map(a=>a.id),d]})};handler.help=["gpinfo"],handler.tags=["casual"],handler.command=/^(gpinfo|infogp|groupinfo)$/i,handler.group=!0;export default handler;
+let handler=async(n,{conn:a,participants:e,groupMetadata:i})=>{const t=await a.profilePictureUrl(n.chat,"image").catch((n=>null))||"./multimedia/imagenes/avatar_contact.png",r=e.filter((n=>n.admin)),d=r.map(((n,a)=>`${a+1}. _@${n.id.split("@")[0]}_`)).join("\n"),l=i.owner||r.find((n=>"superadmin"===n.admin))?.id||n.chat.split`-`[0]+"@s.whatsapp.net",p=e.filter((n=>"admin"===n.admin)).length+e.filter((n=>"superadmin"===n.admin)).length,o=String.fromCharCode(8206).repeat(850),s=`\n*Nombre del grupo* : ${i.subject}\n*Creado por* : _${l.split("@")[0],"@"+l.split("@")[0]}_\n*Fecha de creación* : _${formatDate(1e3*i.creation)}_\n*Total de participantes* : _${e.length}_\n*Total de administradores* : _${p}_\n${d}\n*No administradores* : _${e.filter((n=>null===n.admin)).length}_\n*ID del grupo* : _${i.id}_\n*Descripción* : \n${o}\n${i.desc?.toString()||""}\n`.trim();a.sendFile(n.chat,t,"pp.jpg",s,n,!1,{mentions:[...r.map((n=>n.id)),l]})};handler.help=["gpinfo"],handler.tags=["casual"],handler.command=/^(gpinfo|infogp|groupinfo)$/i,handler.group=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
