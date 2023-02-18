b93b2196e06957336716fae6eedc206dd0b5b854
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 02:41:28 +0000

    @NeKosmic 🙀

--- plugins/info-covid.js
+++ plugins/info-covid.js
@@ -3,20 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(e,{conn:o,args:a})=>{try{let s=await fetchJson(`https://latam-api.vercel.app/api/covid19?apikey=${MyApiKey}&q=${a[0]?a[0]:"world"}`);if(!s.casos)return e.reply("[ ! ] Sin resultados");o.sendMessage(e.chat,{text:`[ COVID-19 ]
-
-🌁 Lugar: ${a[0]?a[0]:"Mundo"}
-🫁 Casos positivos: ${s.casos}
-🤧 Casos de hoy: ${s.casosDehoy}
-💀 Fallecidos: ${s.muertes}
-⚰️ Fallecidos de hoy: ${s.muertesDehoy}
-🤒 Recuperados: ${s.recuperados}
-😷 En recuperacion: ${s.activo}
-😵 Casos criticos: ${s.critico}
-🫂 Casos por millon: ${s.casosPorMillon}
-☠️ Fallecidos por millon: ${s.muertesPorMillon}
-💉 Total de pruebas: ${s.pruebas}
-🔬 Pruebas por millon: ${s.pruebasPorMillon}`},{quoted:e})}catch(r){e.reply(MultiNK.Error0())}};handler.help=["covid"].map(e=>e+" <pais>"),handler.tags=["busqueda"],handler.command=/^(covid|covid19)$/i,handler.limit=!0;export default handler;
+let handler=async(o,{conn:e,args:a})=>{try{const s=await fetchJson(`https://latam-api.vercel.app/api/covid19?apikey=${MyApiKey}&q=${a[0]?a[0]:"world"}`);if(!s.casos)return o.reply("[ ! ] Sin resultados");e.sendMessage(o.chat,{text:`[ COVID-19 ]\n\n🌁 Lugar: ${a[0]?a[0]:"Mundo"}\n🫁 Casos positivos: ${s.casos}\n🤧 Casos de hoy: ${s.casosDehoy}\n💀 Fallecidos: ${s.muertes}\n⚰️ Fallecidos de hoy: ${s.muertesDehoy}\n🤒 Recuperados: ${s.recuperados}\n😷 En recuperacion: ${s.activo}\n😵 Casos criticos: ${s.critico}\n🫂 Casos por millon: ${s.casosPorMillon}\n☠️ Fallecidos por millon: ${s.muertesPorMillon}\n💉 Total de pruebas: ${s.pruebas}\n🔬 Pruebas por millon: ${s.pruebasPorMillon}`},{quoted:o})}catch(e){o.reply(MultiNK.Error0())}};handler.help=["covid <pais>"],handler.tags=["busqueda"],handler.command=/^(covid|covid19)$/i,handler.limit=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
