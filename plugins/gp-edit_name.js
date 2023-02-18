3f88083616f9d1dec94d7ab96ead24c2dd5fcf5d
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 03:44:45 +0000

    @NeKosmic 🙀

--- plugins/gp-edit_name.js
+++ plugins/gp-edit_name.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(e,{conn:a,args:n,text:l})=>l?l.length>=26?e.reply(`*[ ! ] m\xe1ximo de car\xe1cteres es 25*`):void(await a.groupUpdateSubject(e.chat,l),reacMoji(e.chat,a,"✍️",e)):e.reply(`*[ ! ] Ingrese un titulo/nombre/apodo con el cual se actualizar\xe1 el asunto del grupo actual*`);handler.help=["gpname <texto>"],handler.tags=["grupos","admins"],handler.command=/^(gpname|namegp|setname)$/i,handler.group=!0,handler.admin=!0,handler.botAdmin=!0;export default handler;
+let handler=async(e,{conn:a,args:n,text:r})=>r?r.length>=26?e.reply("*[ ! ] máximo de carácteres es 25*"):(await a.groupUpdateSubject(e.chat,r),void reacMoji(e.chat,a,"✍️",e)):e.reply("*[ ! ] Ingrese un titulo/nombre/apodo con el cual se actualizará el asunto del grupo actual*");handler.help=["gpname <texto>"],handler.tags=["grupos","admins"],handler.command=/^(gpname|namegp|setname)$/i,handler.group=!0,handler.admin=!0,handler.botAdmin=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
