64577ad3bfe3aecf4cdbbb04dc80b8dc620b6c92
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 02:55:45 +0000

    @NeKosmic 🙀

--- plugins/hta-rescom.js
+++ plugins/hta-rescom.js
@@ -3,10 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(e,{conn:a,command:t})=>{e.quoted&&e.quoted;let r=(e.quoted?e.quoted:e.msg).mimetype||"";if(!/image|video/.test(r))return e.reply(`Envie \xf3 Responda un video \xf3 imagen con el comando 
-
-${Prefijo+t}
-`);try{var n;await a.sendMessage(e.chat,{text:e.quoted.caption||"[ ! ] No se pudo recuperar un comentario."},{ephemeralExpiration:86400,quoted:e}),reacMoji(e.chat,a,"\uD83D\uDDA8️",e)}catch{e.reply(MultiNK.Error1())}};handler.help=["rescom"],handler.tags=["herramienta"],handler.command=/^(rescom)$/i;export default handler;
+let handler=async(e,{conn:o,command:a})=>{e.quoted&&e.quoted;const n=(e.quoted?e.quoted:e.msg).mimetype||"";if(!/image|video/.test(n))return e.reply(`Envie ó Responda un video ó imagen con el comando \n\n${Prefijo+a}\n`);try{const a=e.quoted.caption||"[ ! ] No se pudo recuperar un comentario.";await o.sendMessage(e.chat,{text:a},{ephemeralExpiration:86400,quoted:e}),reacMoji(e.chat,o,"🖨️",e)}catch{e.reply(MultiNK.Error1())}};handler.help=["rescom"],handler.tags=["herramienta"],handler.command=/^rescom$/i;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
