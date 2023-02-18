19fe0a88055a9ab66f7dc1f90d20a17ec9b55157
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 02:22:20 +0000

    @NeKosmic 🙀

--- plugins/juego-3EnLinea_del.js
+++ plugins/juego-3EnLinea_del.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(e,{conn:a,usedPrefix:l,command:t})=>{let d=Object.values(a.game).find(a=>a.id.startsWith("tictactoe")&&[a.game.playerX,a.game.playerO].includes(e.sender));void 0!=d&&(delete a.game[d.id],await e.reply("*Sala 3 en linea eliminado correctamente ✓*"))};handler.help=["delttt"],handler.tags=["games"],handler.command=/^(delttt)$/,handler.group=!0,handler.limit=!0;export default handler;
+let handler=async(e,{conn:a,usedPrefix:l,command:t})=>{const n=Object.values(a.game).find((a=>a.id.startsWith("tictactoe")&&[a.game.playerX,a.game.playerO].includes(e.sender)));null!=n&&(delete a.game[n.id],await e.reply("*Sala 3 en linea eliminado correctamente ✓*"))};handler.help=["delttt"],handler.tags=["games"],handler.command=/^delttt$/,handler.group=!0,handler.limit=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
