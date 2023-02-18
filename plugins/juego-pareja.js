92e12dae4b252d16bc99a99a97b37e5b8d09e605
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 02:09:23 +0000

    @NeKosmic 🙀

--- plugins/juego-pareja.js
+++ plugins/juego-pareja.js
@@ -3,12 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import*as e from"fs";let handler=async(a,{conn:n,participants:i})=>{let l,t=`❥ *Pareja formada* : 
-
- ┏─━─━─━∞◆∞━─━─━─┓
-@${i[Math.floor(Math.random()*i.length)].id.split("@")[0]} ❤️ @${i[Math.floor(Math.random()*i.length)].id.split("@")[0]} 
- ┗─━─━─━∞◆∞━─━─━─┛
-`;setTimeout(()=>{a.reply(t,a.chat,{mentions:n.parseMention(t)})},500),await n.sendMessage(a.chat,{audio:e.readFileSync("./multimedia/sonidos/willy.m4a"),fileName:"NoviosXD.mp3",mimetype:"audio/mpeg",ptt:!0},{quoted:{key:{participant:"0@s.whatsapp.net"},message:{locationMessage:{name:`Esto fue posible gracias a ${await n.getName(a.sender)}`,jpegThumbnail:e.readFileSync("./multimedia/imagenes/logo.jpg")}}}})};handler.help=["emparejar"],handler.tags=["games"],handler.command=/^(emparejar)$/i,handler.group=!0,handler.limit=!0;export default handler;
+import fs from"fs";let handler=async(a,{conn:e,participants:t})=>{const n=`❥ *Pareja formada* : \n\n ┏─━─━─━∞◆∞━─━─━─┓\n@${t[Math.floor(Math.random()*t.length)].id.split("@")[0]} ❤️ @${t[Math.floor(Math.random()*t.length)].id.split("@")[0]} \n ┗─━─━─━∞◆∞━─━─━─┛\n`;setTimeout((()=>{a.reply(n,a.chat,{mentions:e.parseMention(n)})}),500),await e.sendMessage(a.chat,{audio:fs.readFileSync("./multimedia/sonidos/willy.m4a"),fileName:"NoviosXD.mp3",mimetype:"audio/mpeg",ptt:!0},{quoted:{key:{participant:"0@s.whatsapp.net"},message:{locationMessage:{name:`Esto fue posible gracias a ${await e.getName(a.sender)}`,jpegThumbnail:fs.readFileSync("./multimedia/imagenes/logo.jpg")}}}})};handler.help=["emparejar"],handler.tags=["games"],handler.command=/^emparejar$/i,handler.group=!0,handler.limit=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
