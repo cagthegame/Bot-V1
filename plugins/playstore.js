2f6bc1a3011c70af5b0e4541c073700056486fb7
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 00:49:49 +0000

    @NeKosmic 🙀

--- plugins/playstore.js
+++ plugins/playstore.js
@@ -3,16 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(e,{conn:a,text:r,args:i})=>{if(!i[0])return e.reply("Que desea buscar en playstore?");let t=await a.getName(e.sender),l=encodeURIComponent(r),o=e.reply(MultiNK.Bsqd(t));await o;try{let n=await fetchJson(`https://latam-api.vercel.app/api/playstore?apikey=${MyApiKey}&q=${l}`);if(!n.titulo)return e.reply("[ ! ] Sin resultados");let d=await traducIr(encodeURI(n.descripcion));a.sendMessage(e.chat,{image:{url:n.imagen?n.imagen:"https://drive.google.com/uc?export=download&id=1YMbAkiUfZUQQ4wDkarsNOA7izzSiylkB"},caption:`🔍 Resultado: ${n.titulo}
-🧬 Identificador: ${n.id}
-⛓️ Link: ${n.link}
-🖼️ Imagen: ${n.imagen}
-✍️ Desarrollador: ${n.desarrollador}
-📜 Descripcion: ${d||n.descripcion}
-💲 Moneda: ${n.moneda}
-🎭 Gratis: ${n.gratis}
-💸 Precio: ${n.precio}
-📈 Puntuacion: ${n.puntuacion}`},{quoted:e})}catch(s){e.reply(MultiNK.Error0())}};handler.help=["playstore"].map(e=>e+" <busqueda>"),handler.tags=["busqueda"],handler.command=/^(playstore)$/i,handler.limit=!0;export default handler;
+let handler=async(e,{conn:a,text:n,args:r})=>{if(!r[0])return e.reply("Que desea buscar en playstore?");const t=await a.getName(e.sender),i=encodeURIComponent(n),o=e.reply(MultiNK.Bsqd(t));await o;try{const n=await fetchJson(`https://latam-api.vercel.app/api/playstore?apikey=${MyApiKey}&q=${i}`);if(!n.titulo)return e.reply("[ ! ] Sin resultados");const r=await traducIr(encodeURI(n.descripcion));a.sendMessage(e.chat,{image:{url:n.imagen||"https://drive.google.com/uc?export=download&id=1YMbAkiUfZUQQ4wDkarsNOA7izzSiylkB"},caption:`🔍 Resultado: ${n.titulo}\n🧬 Identificador: ${n.id}\n⛓️ Link: ${n.link}\n🖼️ Imagen: ${n.imagen}\n✍️ Desarrollador: ${n.desarrollador}\n📜 Descripcion: ${r||n.descripcion}\n💲 Moneda: ${n.moneda}\n🎭 Gratis: ${n.gratis}\n💸 Precio: ${n.precio}\n📈 Puntuacion: ${n.puntuacion}`},{quoted:e})}catch(a){e.reply(MultiNK.Error0())}};handler.help=["playstore <busqueda>"],handler.tags=["busqueda"],handler.command=/^playstore$/i,handler.limit=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
