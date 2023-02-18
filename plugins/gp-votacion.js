86f545cee58e8e57e03cef3f10760f318df55ace
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 03:14:26 +0000

    @NeKosmic 🙀

--- plugins/gp-votacion.js
+++ plugins/gp-votacion.js
@@ -3,41 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(a,{conn:o,text:t,isAdmin:e,isOwner:n,command:r,groupMetadata:i})=>{if(a.isGroup&&!(e||n))return dfail("admin",a,o);o.vote=o.vote?o.vote:{};let v=await o.getName(a.sender),l=a.chat;if(l in o.vote)return a.reply(`*[ ! ] Aun hay una votacion activa en el grupo*
-_Use el comando_ : ${Prefijo}reiniciarvotos _para crear nueva votacion_`);if(!t)return a.reply(`*Ingrese un motivo para la votacion, ejemplo:*
-
-${Prefijo+r} el admin es god?
-`);if(t.length<7)return a.reply(`El motivo para la votaci\xf3n es muy corta!`);let c=`⚡ ${"@"+a.sender.split("@")[0]} *Comenz\xf3 una nueva votacion en el grupo:* ${i.subject}
-
-_Como participar en la votaci\xf3n :_${"‎".repeat(850)}
-
-┏⊱   ${Prefijo}sivotar
-┗━⊱  Para dar un voto positivo ✔️
-
-┏⊱   ${Prefijo}novotar 
-┗━⊱  Para dar un voto negativo ❌
-
-┏⊱   ${Prefijo}vervotos 
-┗━⊱  Para verificar los votos 👀
-
-┏⊱   ${Prefijo}reiniciarvotos 
-┗━⊱  Para eliminar una votacion activa ♻️`;await a.reply(c,null,{mentions:o.parseMention(c)}),o.vote[l]=[t,[],[]];let s=`
-\`\`\`[ VOTACI\xd3N ]\`\`\`
-
-📋 *Razon:* ${o.vote[a.chat][0]}
-👤 *Encuestador:* ${v}
-
-📝
-┣🫂
-┣━⊱ Votos a favor [✓]
-┋
-┗━━⊱ Total: ${o.vote[a.chat][1].length}
-
-📝
-┣👥
-┣━⊱ Votos en contra [X]
-┋
-┗━━⊱ Total: ${o.vote[a.chat][2].length}`;o.sendButton(a.chat,s,NombreDelBot+` 🔥`,null,[["Si Votar ✔️",`${Prefijo}sivotar`],["No Votar ❌",`${Prefijo}novotar`]],a,{mentions:o.parseMention(s)})};handler.help=["votacion [razon]"],handler.tags=["admins","grupos"],handler.command=/^(votacion|votación)$/i,handler.group=!0;export default handler;
+let handler=async(n,{conn:o,text:a,isAdmin:e,isOwner:t,command:r,groupMetadata:i})=>{if(n.isGroup&&!e&&!t)return dfail("admin",n,o);o.vote=o.vote?o.vote:{};const v=await o.getName(n.sender),l=n.chat;if(l in o.vote)return n.reply(`*[ ! ] Aun hay una votacion activa en el grupo*\n_Use el comando_ : ${Prefijo}reiniciarvotos _para crear nueva votacion_`);if(!a)return n.reply(`*Ingrese un motivo para la votacion, ejemplo:*\n\n${Prefijo+r} el admin es god?\n`);if(a.length<7)return n.reply("El motivo para la votación es muy corta!");const c=`⚡ ${"@"+n.sender.split("@")[0]} *Comenzó una nueva votacion en el grupo:* ${i.subject}\n\n_Como participar en la votación :_${String.fromCharCode(8206).repeat(850)}\n\n┏⊱   ${Prefijo}sivotar\n┗━⊱  Para dar un voto positivo ✔️\n\n┏⊱   ${Prefijo}novotar \n┗━⊱  Para dar un voto negativo ❌\n\n┏⊱   ${Prefijo}vervotos \n┗━⊱  Para verificar los votos 👀\n\n┏⊱   ${Prefijo}reiniciarvotos \n┗━⊱  Para eliminar una votacion activa ♻️`;await n.reply(c,null,{mentions:o.parseMention(c)}),o.vote[l]=[a,[],[]];const s=`\n\`\`\`[ VOTACIÓN ]\`\`\`\n\n📋 *Razon:* ${o.vote[n.chat][0]}\n👤 *Encuestador:* ${v}\n\n📝\n┣🫂\n┣━⊱ Votos a favor [✓]\n┋\n┗━━⊱ Total: ${o.vote[n.chat][1].length}\n\n📝\n┣👥\n┣━⊱ Votos en contra [X]\n┋\n┗━━⊱ Total: ${o.vote[n.chat][2].length}`;o.sendButton(n.chat,s,NombreDelBot+" 🔥",null,[["Si Votar ✔️",`${Prefijo}sivotar`],["No Votar ❌",`${Prefijo}novotar`]],n,{mentions:o.parseMention(s)})};handler.help=["votacion [razon]"],handler.tags=["admins","grupos"],handler.command=/^(votacion|votación)$/i,handler.group=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
