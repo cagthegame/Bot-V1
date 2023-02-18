c4ae97ddb85aa0544347fcada945dabc81864917
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 03:27:45 +0000

    @NeKosmic 🙀

--- plugins/gp-lista.js
+++ plugins/gp-lista.js
@@ -3,13 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import e from"../lib/connection.js";let handler=async(t,{conn:r})=>{let a="";try{for(let[l,n]of Object.entries(e.store.chats).filter(([e,t])=>e.endsWith("@g.us")&&t.isChats))a+=`♻️ ${await r.getName(l)} :
-🔖 _${l} [${n?.metadata?.read_only?"Fuera del grupo":"Dentro del grupo"}]_
-
-`;t.reply(`*Grupos con mayor actividad de ${NombreDelBot} actualmente:*
-
-${a}
-`.trim())}catch(o){t.reply(MultiNK.Error1())}};handler.help=["gplist"],handler.tags=["casual"],handler.command=/^(gplist|listgp|grouplist|listgroups)$/i;export default handler;
+import Connection from"../lib/connection.js";let handler=async(e,{conn:t})=>{let n="";try{for(let[e,r]of Object.entries(Connection.store.chats).filter((([e,t])=>e.endsWith("@g.us")&&t.isChats)))n+=`♻️ ${await t.getName(e)} :\n🔖 _${e} [${r?.metadata?.read_only?"Fuera del grupo":"Dentro del grupo"}]_\n\n`;e.reply(`*Grupos con mayor actividad de ${NombreDelBot} actualmente:*\n\n${n}\n`.trim())}catch(t){e.reply(MultiNK.Error1())}};handler.help=["gplist"],handler.tags=["casual"],handler.command=/^(gplist|listgp|grouplist|listgroups)$/i;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
