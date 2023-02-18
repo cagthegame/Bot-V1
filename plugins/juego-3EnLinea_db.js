cbcc1a4bb7ea34cd5650a5ff6c1505f90d368785
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 02:31:57 +0000

    @NeKosmic 🙀

--- plugins/juego-3EnLinea_db.js
+++ plugins/juego-3EnLinea_db.js
@@ -3,22 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import{format as e}from"util";import t from"../lib/database.js";let debugMode=!1;export async function before(r){let a,i=!1,n=!1,s=!1;this.game=this.game?this.game:{};let m=Object.values(this.game).find(e=>e.id&&e.game&&e.state&&e.id.startsWith("tictactoe")&&[e.game.playerX,e.game.playerO].includes(r.sender)&&"PLAYING"==e.state);if(m){let o=/^(rendirse)$/i.test(r.text)?99:4999;if(!/^([1-9]|rendirse)$/i.test(r.text)||(s=!/^[1-9]$/.test(r.text),r.sender!==m.game.currentTurn&&!s))return!0;if(debugMode&&r.reply("[DEBUG]\n"+require("util").format({isSurrender:s,text:r.text})),!s&&1>(a=m.game.turn(r.sender===m.game.playerO,parseInt(r.text)-1)))return r.reply({"-3":"El juego ha terminado","-2":"Inv\xe1lido","-1":"Posici\xf3n inv\xe1lida",0:"Posici\xf3n inv\xe1lida"}[a]),!0;r.sender===m.game.winner?i=!0:511===m.game.board&&(n=!0);let d=m.game.render().map(e=>({X:"❌",O:"⭕",1:"1️⃣",2:"2️⃣",3:"3️⃣",4:"4️⃣",5:"5️⃣",6:"6️⃣",7:"7️⃣",8:"8️⃣",9:"9️⃣"})[e]);s&&(m.game._currentTurn=r.sender===m.game.playerX,i=!0);let g=s?m.game.currentTurn:m.game.winner,l=`
-${i?`@${(s?m.game.currentTurn:m.game.winner).split("@")[0]} Victoria! (+${o} XP)`:n?"Juego terminado, empate (+99 XP)":`Turno de @${m.game.currentTurn.split("@")[0]}`}
-
-${d.slice(0,3).join("")}
-${d.slice(3,6).join("")}
-${d.slice(6).join("")}
-        
-❌ = @${m.game.playerX.split("@")[0]}
-⭕ = @${m.game.playerO.split("@")[0]}
-
-~Escriba :~
-
-rendirse
-
-~para darse por vencido~
-`.trim(),u=t.data.users;(m.game._currentTurn^s?m.x:m.o)!==r.chat&&(m[m.game._currentTurn^s?"x":"o"]=r.chat),m.x!==m.o&&await this.sendMessage(m.x,{text:l,mentions:this.parseMention(l)},{quoted:r}),await this.sendMessage(m.o,{text:l,mentions:this.parseMention(l)},{quoted:r}),(n||i)&&(u[m.game.playerX].exp+=99,u[m.game.playerO].exp+=99,i&&(u[g].exp+=o-99),debugMode&&r.reply("[DEBUG]\n"+e(m)),delete this.game[m.id])}return!0}
+import{format}from"util";import db from"../lib/database.js";const debugMode=!1;export async function before(e){let t,r=!1,n=!1,a=!1;this.game=this.game?this.game:{};const i=Object.values(this.game).find((t=>t.id&&t.game&&t.state&&t.id.startsWith("tictactoe")&&[t.game.playerX,t.game.playerO].includes(e.sender)&&"PLAYING"==t.state));if(i){const s=/^(rendirse)$/i.test(e.text)?99:4999;if(!/^([1-9]|rendirse)$/i.test(e.text))return!0;if(a=!/^[1-9]$/.test(e.text),e.sender!==i.game.currentTurn&&!a)return!0;if(debugMode&&e.reply("[DEBUG]\n"+require("util").format({isSurrender:a,text:e.text})),!a&&1>(t=i.game.turn(e.sender===i.game.playerO,parseInt(e.text)-1)))return e.reply({"-3":"El juego ha terminado","-2":"Inválido","-1":"Posición inválida",0:"Posición inválida"}[t]),!0;e.sender===i.game.winner?r=!0:511===i.game.board&&(n=!0);const o=i.game.render().map((e=>({X:"❌",O:"⭕",1:"1️⃣",2:"2️⃣",3:"3️⃣",4:"4️⃣",5:"5️⃣",6:"6️⃣",7:"7️⃣",8:"8️⃣",9:"9️⃣"}[e])));a&&(i.game._currentTurn=e.sender===i.game.playerX,r=!0);const m=a?i.game.currentTurn:i.game.winner,d=`\n${r?`@${(a?i.game.currentTurn:i.game.winner).split("@")[0]} Victoria! (+${s} XP)`:n?"Juego terminado, empate (+99 XP)":`Turno de @${i.game.currentTurn.split("@")[0]}`}\n\n${o.slice(0,3).join("")}\n${o.slice(3,6).join("")}\n${o.slice(6).join("")}\n        \n❌ = @${i.game.playerX.split("@")[0]}\n⭕ = @${i.game.playerO.split("@")[0]}\n\n~Escriba :~\n\nrendirse\n\n~para darse por vencido~\n`.trim(),u=db.data.users;(i.game._currentTurn^a?i.x:i.o)!==e.chat&&(i[i.game._currentTurn^a?"x":"o"]=e.chat),i.x!==i.o&&await this.sendMessage(i.x,{text:d,mentions:this.parseMention(d)},{quoted:e}),await this.sendMessage(i.o,{text:d,mentions:this.parseMention(d)},{quoted:e}),(n||r)&&(u[i.game.playerX].exp+=99,u[i.game.playerO].exp+=99,r&&(u[m].exp+=s-99),debugMode&&e.reply("[DEBUG]\n"+format(i)),delete this.game[i.id])}return!0}
 
 /**
 [_>] https://github.com/NeKosmic/
