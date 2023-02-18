df5ff8bccd4abe46658300f79d235f89c663be5d
Matt Meléndez <tslrame369@gmail.com>
2023-02-18 03:43:28 +0000

    @NeKosmic 🙀

--- plugins/gp-encuesta.js
+++ plugins/gp-encuesta.js
@@ -3,9 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(e,{conn:n,text:r,command:a})=>{let l=await n.getName(e.sender),t=[],s=r.split("|");if(!s[1])return e.reply("*[ ! ] Use el formato*\n\n"+Prefijo+a+" Mensaje|elecci\xf3n1|elecci\xf3n2\n");if(s[12])return e.reply("*Ejemplo de uso correcto* :\n\n"+Prefijo+a+" En este grupo hay mas|hombres|mujeres\n");for(let o=1;o<s.length;o++)t.push([s[o]]);let u=`*📊 Encuesta realizada por* : ${l}
-
-*- Mensaje:* ${r.split("|")[0]}`;return n.sendPoll(e.chat,u,t,e)};handler.help=["encuesta"],handler.tags=["grupos"],handler.command=/^(encuesta)$/i,handler.group=!0;export default handler;
+let handler=async(e,{conn:n,text:r,command:a})=>{let t=await n.getName(e.sender),l=[];const o=r.split("|");if(!o[1])return e.reply("*[ ! ] Use el formato*\n\n"+Prefijo+a+" Mensaje|elección1|elección2\n");if(o[12])return e.reply("*Ejemplo de uso correcto* :\n\n"+Prefijo+a+" En este grupo hay mas|hombres|mujeres\n");for(let e=1;e<o.length;e++)l.push([o[e]]);const s=`*📊 Encuesta realizada por* : ${t}\n\n*- Mensaje:* ${r.split("|")[0]}`;return n.sendPoll(e.chat,s,l,e)};handler.help=["encuesta"],handler.tags=["grupos"],handler.command=/^encuesta$/i,handler.group=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
