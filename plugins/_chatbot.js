7f560625a707f88c21dbfdf29a079416fa6a576c
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 04:02:03 +0000

    @NeKosmic 🙀

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import t from"node-fetch";import e from"../lib/database.js";export async function before(a){let i=e.data.chats[a.chat];if(i.simi&&!i.isBanned&&!a.fromMe){if(a.text.startsWith(Prefijo)||!a.text)return;let s=await t(`https://api.simsimi.net/v2/?text=${encodeURIComponent(a.text)}&lc=es`);if(!s.ok)throw"Simi muri\xf3 :v";let r=await s.json();return"No s\xe9 lo qu\xe9 est\xe1s diciendo. Por favor ense\xf1ame."==r.success&&await a.reply("No habla taka taka x'd"),await a.reply(r.success),!0}return!0}
+import fetch from"node-fetch";import db from"../lib/database.js";const defaultLang=MultiNK.Lengua();export async function before(t){const e=db.data.chats[t.chat];if(e.simi&&!e.isBanned&&!t.fromMe){if(t.text.startsWith(Prefijo))return;if(!t.text)return;const e=await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(t.text)}&lc=${defaultLang}`);if(!e.ok)throw"Simi murió :v";const a=await e.json();return"No sé lo qué estás diciendo. Por favor enseñame."==a.success&&await t.reply("No habla taka taka x'd"),await t.reply(a.success),!0}return!0}
 
 /**
 [_>] https://github.com/NeKosmic/
