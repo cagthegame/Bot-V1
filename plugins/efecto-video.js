ae71755eb7d1fe4b6cb401947406eed590f8a579
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 01:30:39 +0000

    @NeKosmic 🙀

--- plugins/efecto-video.js
+++ plugins/efecto-video.js
@@ -3,11 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import{unlinkSync as e,readFileSync as a}from"fs";import{join as t}from"path";import{exec as r}from"child_process";import*as o from"fs";let handler=async(o,{conn:i,args:d,__dirname:l,command:n})=>{let s=await i.getName(o.sender);try{let m=o.quoted?o.quoted:o,p=(o.quoted?o.quoted:o.msg).mimetype||"",v;if(/acelerarvid/.test(n)&&(v=['-filter_complex "[0:v]setpts=0.5*PTS[v];[0:a]atempo=2[a]" -map "[v]" -map "[a]"']),/lentovid/.test(n)&&(v=['-filter_complex "[0:v]setpts=2*PTS[v];[0:a]atempo=0.5[a]" -map "[v]" -map "[a]"']),/reversavid/.test(n)&&(v=["-vf reverse -af areverse"]),!/video/.test(p))return o.reply(`Envie \xf3 Responda un v\xeddeo, con el comando 
-
-${Prefijo+n}
-`);{let c=getRandom(".mp4"),f=t(l,"../tmp/"+c),h=await m.download(!0);r(`ffmpeg -i ${h} ${v[0]} ${f}`,async(t,r,d)=>{if(await e(h),t)return o.reply(MultiNK.Error1());await i.sendMessage(o.chat,{video:await a(f),caption:`┏━━⊱ ${n} 
-┗⊱ By ${s||NombreDelBot}`})}),reacMoji(o.chat,i,"⚙️",o)}}catch($){throw $}};handler.help=["acelerarvid","lentovid","reversavid"],handler.tags=["conversor"],handler.command=/^(acelerarvid|lentovid|reversavid)$/i,handler.limit=!0;export default handler;
+import{unlinkSync,readFileSync}from"fs";import{join}from"path";import{exec}from"child_process";import*as fs from"fs";let handler=async(e,{conn:a,args:t,__dirname:r,command:o})=>{const n=await a.getName(e.sender);try{const t=e.quoted?e.quoted:e,i=(e.quoted?e.quoted:e.msg).mimetype||"";let d;if(/acelerarvid/.test(o)&&(d=['-filter_complex "[0:v]setpts=0.5*PTS[v];[0:a]atempo=2[a]" -map "[v]" -map "[a]"']),/lentovid/.test(o)&&(d=['-filter_complex "[0:v]setpts=2*PTS[v];[0:a]atempo=0.5[a]" -map "[v]" -map "[a]"']),/reversavid/.test(o)&&(d=["-vf reverse -af areverse"]),!/video/.test(i))return e.reply(`Envie ó Responda un vídeo, con el comando \n\n${Prefijo+o}\n`);{const i=getRandom(".mp4"),s=join(r,"../tmp/"+i),l=await t.download(!0);exec(`ffmpeg -i ${l} ${d[0]} ${s}`,(async(t,r,i)=>{if(await unlinkSync(l),t)return e.reply(MultiNK.Error1());const d=await readFileSync(s);await a.sendMessage(e.chat,{video:d,caption:`┏━━⊱ ${o} \n┗⊱ By ${n||NombreDelBot}`})})),reacMoji(e.chat,a,"⚙️",e)}}catch(e){throw e}};handler.help=["acelerarvid","lentovid","reversavid"],handler.tags=["conversor"],handler.command=/^(acelerarvid|lentovid|reversavid)$/i,handler.limit=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
