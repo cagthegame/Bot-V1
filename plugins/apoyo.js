f6d0f8dfa1d7a6f292e1a00d69e74ddb3f8af594
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 03:07:18 +0000

    @NeKosmic 🙀

--- plugins/apoyo.js
+++ plugins/apoyo.js
@@ -3,9 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import{generateWAMessageFromContent as e,generateWAMessage as a}from"@adiwajshing/baileys";import*as s from"fs";let handler=async(e,{conn:a})=>{await a.relayMessage(e.chat,{requestPaymentMessage:{noteMessage:{extendedTextMessage:{text:`💻 *Redes sociales* 📲
-
-${"‎".repeat(850)+TusRedesSociales}`,currencyCodeIso4217:"PEN",requestFrom:"0@s.whatsapp.net",expiryTimestamp:8600,amount:1e4,background:s.readFileSync("./multimedia/imagenes/thx.jpg")}}}},{}),reacMoji(e.chat,a,"❤️",e)};handler.help=["apoyo"],handler.tags=["casual"],handler.command=/^(apoyo|apoyar|donar|donasi|dono)$/i,handler.exp=100;export default handler;
+import{generateWAMessageFromContent,generateWAMessage}from"@adiwajshing/baileys";import fs from"fs";let handler=async(e,{conn:a})=>{const s=String.fromCharCode(8206).repeat(850);await a.relayMessage(e.chat,{requestPaymentMessage:{noteMessage:{extendedTextMessage:{text:`💻 *Redes sociales* 📲\n\n${s+TusRedesSociales}`,currencyCodeIso4217:"PEN",requestFrom:"0@s.whatsapp.net",expiryTimestamp:8600,amount:1e4,background:fs.readFileSync("./multimedia/imagenes/thx.jpg")}}}},{}),reacMoji(e.chat,a,"❤️",e)};handler.help=["apoyo"],handler.tags=["casual"],handler.command=/^(apoyo|apoyar|donar|donasi|dono)$/i,handler.exp=100;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
