c4ce96fbce30349ee65ea8d145c3cc74b3ec432c
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 03:26:16 +0000

    @NeKosmic 🙀

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -3,12 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import a from"../lib/database.js";let handler=async(e,{text:t,conn:o})=>{let r=a.data.users[e.sender];return t?t.length<10?e.reply("[ ! ] El motivo es muy corto"):void(r.afk=+new Date,r.afkReason=t,await o.sendMessage(e.chat,{text:`*Se activo la funci\xf3n AFK exitosamente*
-
-➸ *Usuario*: ${o.getName(e.sender)}
-➸ *Razon*: ${t}`},{quoted:e}),reacMoji(e.chat,o,"\uD83D\uDCA4",e)):e.reply(`Por favor diga su motivo para irse AFK
-Ejemplo de uso : 
-${Prefijo}afk ir\xe9 al ba\xf1o :v`)};handler.help=["afk [razon]"],handler.tags=["casual"],handler.command=/^afk$/i;export default handler;
+import db from"../lib/database.js";let handler=async(a,{text:e,conn:n})=>{const o=db.data.users[a.sender];return e?e.length<10?a.reply("[ ! ] El motivo es muy corto"):(o.afk=+new Date,o.afkReason=e,await n.sendMessage(a.chat,{text:`*Se activo la función AFK exitosamente*\n\n➸ *Usuario*: ${n.getName(a.sender)}\n➸ *Razon*: ${e}`},{quoted:a}),void reacMoji(a.chat,n,"💤",a)):a.reply(`Por favor diga su motivo para irse AFK\nEjemplo de uso : \n${Prefijo}afk iré al baño :v`)};handler.help=["afk [razon]"],handler.tags=["casual"],handler.command=/^afk$/i;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
