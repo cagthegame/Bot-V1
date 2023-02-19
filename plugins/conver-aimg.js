08187b70fd6540f8e2a8b21187014abd690e9b59
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 02:46:52 +0000

    @NeKosmic 🙀

--- plugins/conver-aimg.js
+++ plugins/conver-aimg.js
@@ -3,13 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import{spawn as e}from"child_process";import{format as t}from"util";let handler=async(n,{conn:r,usedPrefix:o,command:a})=>{if(!global.support.convert&&!global.support.magick&&!global.support.gm)return handler.disabled=!1;if(!n.quoted)return n.reply(`*Respomda una imagen usando el comando* :
-
-${o+a}
-`);let i=n.quoted;if(!/sticker/.test(i.mediaType))return n.reply(`*Respomda una imagen usando el comando* :
-
-${o+a}
-`);{let d=await i.download();if(!d)throw d;if(!1==!i.isAnimated)return n.reply(`[ ! ] Solo stickers est\xe1ticos`);let s=[],[p,...l]=[...global.support.gm?["gm"]:global.support.magick?["magick"]:[],"convert","webp:-","png:-"],m=e(p,l);m.on("error",e=>n.reply(t(e))),m.stdout.on("data",e=>s.push(e)),m.stdin.write(d),m.stdin.end(),m.on("exit",()=>{r.sendFile(n.chat,Buffer.concat(s),"image.png",NombreDelBot,n),reacMoji(n.chat,r,"⚙️",n)})}};handler.help=["aimg"],handler.tags=["conversor"],handler.command=/^(aimg|toimg)$/i;export default handler;
+import{spawn}from"child_process";import{format}from"util";let handler=async(e,{conn:o,usedPrefix:a,command:n})=>{if(!global.support.convert&&!global.support.magick&&!global.support.gm)return handler.disabled=!1;if(!e.quoted)return e.reply(`*Respomda una imagen usando el comando* :\n\n${a+n}\n`);const t=e.quoted;if(!/sticker/.test(t.mediaType))return e.reply(`*Respomda una imagen usando el comando* :\n\n${a+n}\n`);{let a=await t.download();if(!a)throw a;if(0==!t.isAnimated)return e.reply("[ ! ] Solo stickers estáticos");let n=[];const[r,...i]=[...global.support.gm?["gm"]:global.support.magick?["magick"]:[],"convert","webp:-","png:-"],l=spawn(r,i);l.on("error",(o=>e.reply(format(o)))),l.stdout.on("data",(e=>n.push(e))),l.stdin.write(a),l.stdin.end(),l.on("exit",(()=>{o.sendFile(e.chat,Buffer.concat(n),"image.png",NombreDelBot,e),reacMoji(e.chat,o,"⚙️",e)}))}};handler.help=["aimg"],handler.tags=["conversor"],handler.command=/^(aimg|toimg)$/i;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
