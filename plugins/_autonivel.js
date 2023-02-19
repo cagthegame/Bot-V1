56edf84ad1bdb50930746a242cef110d9ca7df4c
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 04:03:29 +0000

    @NeKosmic 🙀

--- plugins/_autonivel.js
+++ plugins/_autonivel.js
@@ -3,12 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import e from"../lib/database.js";import{canLevelUp as l}from"../lib/levelling.js";export function before(r){let i=e.data.users[r.sender];if(!i.autolevelup)return!0;let t=1*i.level;for(;l(i.level,i.exp,global.multiplier);)i.level++;t!==i.level&&(i.role=global.rpg.role(i.level).name,r.reply(`
-Felicidades, ${this.getName(r.sender)} subiste de nivel!
-♻️ Nivel anterior : ${t}
-🪀 Nivel actual : ${i.level}
-⚔️ Rol : ${i.role}
-	`.trim()))}export const disabled=!0;
+import db from"../lib/database.js";import{canLevelUp}from"../lib/levelling.js";export function before(e){const l=db.data.users[e.sender];if(!l.autolevelup)return!0;const r=1*l.level;for(;canLevelUp(l.level,l.exp,global.multiplier);)l.level++;r!==l.level&&(l.role=global.rpg.role(l.level).name,e.reply(`\nFelicidades, ${this.getName(e.sender)} subiste de nivel!\n♻️ Nivel anterior : ${r}\n🪀 Nivel actual : ${l.level}\n⚔️ Rol : ${l.role}\n\t`.trim()))}export const disabled=!0;
 
 /**
 [_>] https://github.com/NeKosmic/
