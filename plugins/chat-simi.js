0c510154af030735ee3799526b8abe264a297204
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 02:50:35 +0000

    @NeKosmic 🙀

--- plugins/chat-simi.js
+++ plugins/chat-simi.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import a from"../lib/database.js";let handler=async(e,{text:t,command:r,conn:l})=>{let s=await l.getName(e.sender),i="sk-XRL8VTi1CYcuuVbabZsofT3BlbkFJ9YSDTGK0VMnssFw5xFtx",c="No hay sistema!";if(a.data.chats[e.chat].soloAdmins){if(t.length<15)return e.reply(`Por favor, sea mas espec\xedfico en su mensaje`);try{e.reply(MultiNK.Proces(s));await e.reply(await chatBotAI(e.chat,t,i))}catch(h){e.reply(c)}}else{if(!t)return e.reply("Y el texto ...?");try{let n=await chatBotAI(e.chat,t,i);e.reply(n)}catch(o){e.reply(c)}}};handler.help=["simi"].map(a=>a+" <texto>"),handler.tags=[""],handler.command=/^(simi|bot|aichat)$/i;export default handler;
+import db from"../lib/database.js";let handler=async(a,{text:t,command:e,conn:r})=>{const s=await r.getName(a.sender),c="No hay sistema!";if(db.data.chats[a.chat].soloAdmins){if(t.length<15)return a.reply("Por favor, sea mas específico en su mensaje");try{a.reply(MultiNK.Proces(s));const e=await chatBotAI(a.chat,t,"");await a.reply(e)}catch(t){a.reply(c)}}else{if(!t)return a.reply("Y el texto ...?");try{const e=await chatBotAI(a.chat,t,"");a.reply(e)}catch(t){a.reply(c)}}};handler.help=["simi <texto>"],handler.tags=[""],handler.command=/^(simi|bot|aichat)$/i;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
