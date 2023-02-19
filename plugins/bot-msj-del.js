2b4f3f83b5175596b8c4b00d97b1c1724ff38095
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 02:56:13 +0000

    @NeKosmic 🙀

--- plugins/bot-msj-del.js
+++ plugins/bot-msj-del.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=function(e,{conn:t,isAdmin:a,isBotAdmin:d}){if(!e.quoted)throw!1;let{isBaileys:s}=e.quoted,n=e.message.extendedTextMessage.contextInfo.participant,i=e.message.extendedTextMessage.contextInfo.stanzaId;if(d&&a)t.sendMessage(e.chat,{delete:{remoteJid:e.chat,fromMe:!1,id:i,participant:n}});else if(a){if(!s)throw!1;t.sendMessage(e.chat,{delete:e.quoted.vM.key})}else{if(!s)throw!1;t.sendMessage(e.chat,{delete:e.quoted.vM.key}),e.limit=1}};handler.help=["suprimir"],handler.tags=["grupos","admins"],handler.command=/^(suprimir|del)$/i;export default handler;
+let handler=function(e,{conn:t,isAdmin:s,isBotAdmin:d}){if(!e.quoted)throw!1;const{isBaileys:a}=e.quoted,i=e.message.extendedTextMessage.contextInfo.participant,n=e.message.extendedTextMessage.contextInfo.stanzaId;if(d&&s)t.sendMessage(e.chat,{delete:{remoteJid:e.chat,fromMe:!1,id:n,participant:i}});else if(s){if(!a)throw!1;t.sendMessage(e.chat,{delete:e.quoted.vM.key})}else{if(!a)throw!1;t.sendMessage(e.chat,{delete:e.quoted.vM.key}),e.limit=1}};handler.help=["suprimir"],handler.tags=["grupos","admins"],handler.command=/^(suprimir|del)$/i;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
