d4afd882bfd255b15253fc02cf3326850c69d353
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 02:39:52 +0000

    @NeKosmic 🙀

--- plugins/conver-texto.js
+++ plugins/conver-texto.js
@@ -3,11 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import t from"cheerio";import e from"axios";let handler=async(t,{conn:e,text:r})=>{if(!r)return t.reply("Y el texto?");let n=await e.getName(t.sender);encodeURIComponent(r);let a=t.reply(MultiNK.Proces(n));await a;try{let l=await fuentes(r),o=`*[_>] Estilos de texto para:* _${r}_
-
-`;for(let i of l)o+=`✍️ *${i.name}* : ${i.result}
-
-`;t.reply(o)}catch(d){t.reply(MultiNK.Error0())}};handler.help=["txtestilo"],handler.tags=["conversor"],handler.command=/^(txtestilo|fuentes)$/i;export default handler;function fuentes(r){return new Promise((n,a)=>{e.get("http://qaz.wtf/u/convert.cgi?text="+r).then(({data:e})=>{let r=t.load(e),a=[];r("table > tbody > tr").each(function(t,e){a.push({name:r(e).find("td:nth-child(1) > span").text(),result:r(e).find("td:nth-child(2)").text().trim()})}),n(a)}).catch(t=>a(t))})}
+import cheerio from"cheerio";import axios from"axios";let handler=async(t,{conn:e,text:n})=>{if(!n)return t.reply("Y el texto?");const r=await e.getName(t.sender),o=(encodeURIComponent(n),t.reply(MultiNK.Proces(r)));await o;try{const e=await fuentes(n);let r=`*[_>] Estilos de texto para:* _${n}_\n\n`;for(let t of e)r+=`✍️ *${t.name}* : ${t.result}\n\n`;t.reply(r)}catch(e){t.reply(MultiNK.Error0())}};handler.help=["txtestilo"],handler.tags=["conversor"],handler.command=/^(txtestilo|fuentes)$/i;export default handler;function fuentes(t){return new Promise(((e,n)=>{axios.get("http://qaz.wtf/u/convert.cgi?text="+t).then((({data:t})=>{let n=cheerio.load(t),r=[];n("table > tbody > tr").each((function(t,e){r.push({name:n(e).find("td:nth-child(1) > span").text(),result:n(e).find("td:nth-child(2)").text().trim()})})),e(r)})).catch((t=>n(t)))}))}
 
 /**
 [_>] https://github.com/NeKosmic/
