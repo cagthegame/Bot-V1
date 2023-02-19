758bd2c24b290efd1c6ac9e6294f0796864e756f
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 03:52:03 +0000

    @NeKosmic 🙀

--- plugins/_getmsg.js
+++ plugins/_getmsg.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import t from"../lib/database.js";export async function all(a){if(!a.chat.endsWith(".net")||a.fromMe||a.key.remoteJid.endsWith("status@broadcast")||t.data.chats[a.chat].isBanned||t.data.users[a.sender].banned||a.isBaileys)return;let e=t.data.msgs;if(a.text in e)await this.serializeM(JSON.parse(JSON.stringify(e[a.text]),(t,a)=>null!==a&&"object"==typeof a&&"type"in a&&"Buffer"===a.type&&"data"in a&&Array.isArray(a.data)?Buffer.from(a.data):a)).copyNForward(a.chat,!0)}
+import db from"../lib/database.js";export async function all(t){if(!t.chat.endsWith(".net")||t.fromMe||t.key.remoteJid.endsWith("status@broadcast"))return;if(db.data.chats[t.chat].isBanned)return;if(db.data.users[t.sender].banned)return;if(t.isBaileys)return;const a=db.data.msgs;if(!(t.text in a))return;let e=this.serializeM(JSON.parse(JSON.stringify(a[t.text]),((t,a)=>null!==a&&"object"==typeof a&&"type"in a&&"Buffer"===a.type&&"data"in a&&Array.isArray(a.data)?Buffer.from(a.data):a)));await e.copyNForward(t.chat,!0)}
 
 /**
 [_>] https://github.com/NeKosmic/
