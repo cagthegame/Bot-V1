6b648cf6e10646ef3432e36ab4b0033813ef63db
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 01:49:17 +0000

    @NeKosmic 🙀

--- plugins/dl-ytplay.js
+++ plugins/dl-ytplay.js
@@ -3,22 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import{generateWAMessageFromContent as e}from"@adiwajshing/baileys";import a from"../lib/database.js";let handler=async(t,{conn:r,command:o,text:l})=>{try{let i=await fetchJson(`https://latam-api.vercel.app/api/ytplay2?apikey=${MyApiKey}&q=${encodeURIComponent(l)}`),n=1e3*i.duracion_s,s=a.data.users[t.sender];if(new Date-s.lagnet<n)return t.reply(`_Por favor espere ${clockString(s.lagnet+n-new Date)}antes de realizar otra solicitud_ 
-~ᴱˡ ᵗᶦᵉᵐᵖᵒ ᵈᵉ ᵉˢᵖᵉʳᵃ ᵈᵉᵖᵉⁿᵈᵉ ᵈᵉ ˡᵃ ᵈᵘʳᵃᶜᶦᵒ́ⁿ ᵈᵉˡ ᵃᵘᵈᶦᵒ ᵃⁿᵗᵉʳᶦᵒʳᵐᵉⁿᵗᵉ ˢᵒˡᶦᶜᶦᵗᵃᵈᵒ~`);if(!l)return t.reply(`Que desea buscar en Youtube?, Ejemplo de uso: 
-
-${Prefijo+o} mtc s3rl`);let d;await t.reply(MultiNK.Proces(await r.getName(t.sender)));let u=await getBuffer(i.logo),p=e(t.chat,{extendedTextMessage:{text:`
-*✍️ T\xedtulo:* ${i.titulo}
-*🎹 Autor:* ${i.autor}
-*🗜️ Tama\xf1o:* ${i.peso+" Aprox."}
-*⏳ Duraci\xf3n:* ${i.duracion}
-
-
-
- _Enviando audio, espere..._
-`.trim(),contextInfo:{externalAdReply:{title:i.titulo,body:"⇆ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤ↻",thumbnail:u,sourceUrl:"https://youtube.com/channel/UC_Pp8pT9vbT15k5_-i6oseA?sub_confirmation=1"}}}},{quoted:t});await r.relayMessage(t.chat,p.message,{messageId:p.key.id}),s.lagnet=new Date*1,r.sendMessage(t.chat,{audio:{url:decodeURIComponent(i.descarga1)||i.descarga2},mimetype:"audio/mp4",fileName:`${i.titulo}.mp3`},{quoted:t}).catch(e=>{t.reply(`Ocurrio un error, por favor use el comando:
-
-${Prefijo}audio ${l}
-`)})}catch(c){t.reply(MultiNK.Error0())}};handler.help=["play <texto>"],handler.tags=["servicio"],handler.command=/^play$/i;export default handler;
+import{generateWAMessageFromContent}from"@adiwajshing/baileys";import db from"../lib/database.js";let handler=async(e,{conn:a,command:t,text:n})=>{try{const o=encodeURIComponent(n),r=await fetchJson(`https://latam-api.vercel.app/api/ytplay2?apikey=${MyApiKey}&q=${o}`),i=1e3*r.duracion_s,s=db.data.users[e.sender];if(new Date-s.lagnet<i)return e.reply(`_Por favor espere ${clockString(s.lagnet+i-new Date)}antes de realizar otra solicitud_ \n~ᴱˡ ᵗᶦᵉᵐᵖᵒ ᵈᵉ ᵉˢᵖᵉʳᵃ ᵈᵉᵖᵉⁿᵈᵉ ᵈᵉ ˡᵃ ᵈᵘʳᵃᶜᶦᵒ́ⁿ ᵈᵉˡ ᵃᵘᵈᶦᵒ ᵃⁿᵗᵉʳᶦᵒʳᵐᵉⁿᵗᵉ ˢᵒˡᶦᶜᶦᵗᵃᵈᵒ~`);if(!n)return e.reply(`Que desea buscar en Youtube?, Ejemplo de uso: \n\n${Prefijo+t} mtc s3rl`);const l=await a.getName(e.sender),d=e.reply(MultiNK.Proces(l));await d;const c=await getBuffer(r.logo),u=generateWAMessageFromContent(e.chat,{extendedTextMessage:{text:`\n*✍️ Título:* ${r.titulo}\n*🎹 Autor:* ${r.autor}\n*🗜️ Tamaño:* ${r.peso+" Aprox."}\n*⏳ Duración:* ${r.duracion}\n\n\n\n _Enviando audio, espere..._\n`.trim(),contextInfo:{externalAdReply:{title:r.titulo,body:"⇆ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤ↻",thumbnail:c,sourceUrl:"https://youtube.com/channel/UC_Pp8pT9vbT15k5_-i6oseA?sub_confirmation=1"}}}},{quoted:e});await a.relayMessage(e.chat,u.message,{messageId:u.key.id}),s.lagnet=1*new Date,a.sendMessage(e.chat,{audio:{url:decodeURIComponent(r.descarga1)||r.descarga2},mimetype:"audio/mp4",fileName:`${r.titulo}.mp3`},{quoted:e}).catch((a=>{e.reply(`Ocurrio un error, por favor use el comando:\n\n${Prefijo}audio ${n}\n`)}))}catch(a){e.reply(MultiNK.Error0())}};handler.help=["play <texto>"],handler.tags=["servicio"],handler.command=/^play$/i;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
