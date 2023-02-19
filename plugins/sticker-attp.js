3127e8d952e0698b4433bc8c2adca2a7c68b1764
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 19:14:45 +0000

    @NeKosmic 🙀

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import webp from"node-webpmux";import{randomBytes}from"crypto";import{addExif}from"../lib/sticker.js";let handler=async(t,{conn:e,text:r,command:o})=>{if(!r)return t.reply(`Modo de uso:\n\n${Prefijo+o} The life is a party\n`);const a=encodeURIComponent(r);try{const r=await getBuffer(`https://api.xteam.xyz/attp?file&text=${a}`),o=await addExif(r,"",`[_>] ${NombreDelBot}\n`);e.sendMessage(t.chat,{sticker:o},{quoted:t})}catch(e){t.reply(MultiNK.Error0())}};handler.help=["attp <texto>"],handler.tags=["conversor"],handler.command=/^attp$/i,handler.limit=!0;export default handler;const randomID=t=>randomBytes(Math.ceil(.5*t)).toString("hex").slice(0,t);export default handler;
+import webp from"node-webpmux";import{randomBytes}from"crypto";import{addExif}from"../lib/sticker.js";let handler=async(t,{conn:e,text:r,command:o})=>{if(!r)return t.reply(`Modo de uso:\n\n${Prefijo+o} The life is a party\n`);const a=encodeURIComponent(r);try{const r=await getBuffer(`https://api.xteam.xyz/attp?file&text=${a}`),o=await addExif(r,"",`[_>] ${NombreDelBot}\n`);e.sendMessage(t.chat,{sticker:o},{quoted:t})}catch(e){t.reply(MultiNK.Error0())}};handler.help=["attp <texto>"],handler.tags=["conversor"],handler.command=/^attp$/i,handler.limit=!0;export default handler;const randomID=t=>randomBytes(Math.ceil(.5*t)).toString("hex").slice(0,t);
 
 /**
 [_>] https://github.com/NeKosmic/
