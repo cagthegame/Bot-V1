5618e75fcbad31b28ddad6e9493262f5bc7451a0
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 18:59:53 +0000

    @NeKosmic 🙀

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -3,10 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import e from"../lib/database.js";let linkRegex=/chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i;export async function before(t,{conn:a,isAdmin:i,isBotAdmin:n,usedPrefix:s}){if(t.isBaileys&&t.fromMe)return!0;if(!t.isGroup)return!1;let r=e.data.chats[t.chat];e.data.settings[this.user.jid];let o=t.key.participant,c=t.key.id,d=linkRegex.exec(t.text);if(r.antiLink&&d&&!i){if(n){let p=`https://chat.whatsapp.com/${await this.groupInviteCode(t.chat)}`;if(t.text.includes(p))return t.reply("[ ! ] Aea causa acabas de enviar un enlace, lo weno es que el enlace detectado es de este grupo owo")}if(!n)return a.sendButton(t.chat,`*[ ! ] Enlace detectado [ ! ]*
-`,"Por suerte no soy acmin, asi que no puedo hacer nada :v",NombreDelBot,["[ DESACTIVAR ANTILINK ]",s+"apagar antilink"],t);await a.sendMessage(t.chat,{text:`*[ ! ] Enlace detectado [ ! ]*
-_Ahora si k_gaste @${t.sender.split("@")[0]} Adios..._
-`,mentions:[t.sender]}),a.sendMessage(t.chat,{delete:{remoteJid:t.chat,fromMe:!1,id:c,participant:o}}),setTimeout(()=>{a.groupParticipantsUpdate(t.chat,[t.sender],"remove")},1e3)}return!0}
+import db from"../lib/database.js";const linkRegex=/chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i;export async function before(e,{conn:t,isAdmin:a,isBotAdmin:i,usedPrefix:n}){if(e.isBaileys&&e.fromMe)return!0;if(!e.isGroup)return!1;const s=db.data.chats[e.chat],o=(db.data.settings[this.user.jid],e.key.participant),r=e.key.id,c=linkRegex.exec(e.text);if(s.antiLink&&c&&!a){if(i){const t=`https://chat.whatsapp.com/${await this.groupInviteCode(e.chat)}`;if(e.text.includes(t))return e.reply("[ ! ] Aea causa acabas de enviar un enlace, lo weno es que el enlace detectado es de este grupo owo")}if(!i)return t.sendButton(e.chat,"*[ ! ] Enlace detectado [ ! ]*\n","Por suerte no soy acmin, asi que no puedo hacer nada :v",NombreDelBot,["[ DESACTIVAR ANTILINK ]",n+"apagar antilink"],e);await t.sendMessage(e.chat,{text:`*[ ! ] Enlace detectado [ ! ]*\n_Ahora si k_gaste @${e.sender.split("@")[0]} Adios..._\n`,mentions:[e.sender]}),t.sendMessage(e.chat,{delete:{remoteJid:e.chat,fromMe:!1,id:r,participant:o}}),setTimeout((()=>{t.groupParticipantsUpdate(e.chat,[e.sender],"remove")}),1e3)}return!0}
 
 /**
 [_>] https://github.com/NeKosmic/
