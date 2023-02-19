d466a50267ded508119d7844bc355d66637ac1e0
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 01:42:42 +0000

    @NeKosmic 🙀

--- plugins/dl-ytv.js
+++ plugins/dl-ytv.js
@@ -3,9 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import*as e from"fs";let handler=async(a,{conn:t,command:r,text:l,args:i})=>{if(!l)return a.reply(`Que desea descargar de Youtube?, Ejemplo de uso: 
-
-${Prefijo+r} https://youtu.be/cNgyuHtBBW8`);if(!isUrl(i[0])&&!i[0].includes("youtu"))return reply("*[ ! ] Link inv\xe1lido*\n_Por favor, use un link de YouTube_\n");let n;await a.reply(MultiNK.Proces(await t.getName(a.sender)));let d=encodeURIComponent(l);try{let o=await fetchJson(`https://latam-api.vercel.app/api/ytmp4?apikey=${MyApiKey}&q=${d}`);await t.sendMessage(a.chat,{document:{url:o.descarga},mimetype:"video/mp4",fileName:`${o.titulo}.mp4`,jpegThumbnail:e.readFileSync("./multimedia/imagenes/logo.jpg")},{quoted:a}).catch(e=>{a.reply(MultiNK.Error1())})}catch(u){a.reply(MultiNK.Error0())}};handler.help=["ytv <link>"],handler.tags=["servicio"],handler.command=/^(ytv)$/i,handler.limit=!0;export default handler;
+import fs from"fs";let handler=async(e,{conn:t,command:a,text:r,args:i})=>{if(!r)return e.reply(`Que desea descargar de Youtube?, Ejemplo de uso: \n\n${Prefijo+a} https://youtu.be/cNgyuHtBBW8`);if(!isUrl(i[0])&&!i[0].includes("youtu"))return reply("*[ ! ] Link inválido*\n_Por favor, use un link de YouTube_\n");const n=await t.getName(e.sender),l=e.reply(MultiNK.Proces(n));await l;const o=encodeURIComponent(r);try{const a=await fetchJson(`https://latam-api.vercel.app/api/ytmp4?apikey=${MyApiKey}&q=${o}`);await t.sendMessage(e.chat,{document:{url:a.descarga},mimetype:"video/mp4",fileName:`${a.titulo}.mp4`,jpegThumbnail:fs.readFileSync("./multimedia/imagenes/logo.jpg")},{quoted:e}).catch((t=>{e.reply(MultiNK.Error1())}))}catch(t){e.reply(MultiNK.Error0())}};handler.help=["ytv <link>"],handler.tags=["servicio"],handler.command=/^ytv$/i,handler.limit=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
