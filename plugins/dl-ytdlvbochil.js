037ccecda7064ce8f4051228118e357fbf5b89d7
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 02:04:29 +0000

    @NeKosmic 🙀

--- plugins/dl-ytdlvbochil.js
+++ plugins/dl-ytdlvbochil.js
@@ -3,11 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import{youtubedl as e,youtubedlv2 as a,youtubedlv3 as i}from"@bochilteam/scraper";let handler=async(t,{conn:r,args:o})=>{if(!o[0])return t.reply(`Que desea descargar de Youtube?, Ejemplo de uso: 
-
-${Prefijo+command} https://youtu.be/ed-6VSF-GGc`);if(!isUrl(o[0])&&!o[0].includes("youtu"))return reply("*[ ! ] Link inv\xe1lido*\n_Por favor, use un link de YouTube_\n");let n;await t.reply(MultiNK.Proces(await r.getName(t.sender)));try{let l=(o[1]||"360")+"p",d=o[0],c=await e(d).catch(async()=>await a(d)).catch(async()=>await i(d)),s=await c.video[l].download(),u=await c.title,p=await c.video[l].fileSizeH;if(p.split("MB")[0]>=50)return t.reply("[ ! ] Tu wea pesa demasiado, sorry mi king no podre enviarlo :v");await r.sendMessage(t.chat,{video:{url:s},fileName:`${u}.mp4`,mimetype:"video/mp4",caption:`Titulo: ${u}
-Tama\xf1o: ${p}
-Extencion: .mp4`},{quoted:t})}catch{await r.reply(t.chat,MultiNK.Error1(),t)}};handler.help=["ytvbochi <link>"],handler.tags=["servicio"],handler.command=/^(ytvbochi)$/i;export default handler;
+import{youtubedl,youtubedlv2,youtubedlv3}from"@bochilteam/scraper";let handler=async(e,{conn:a,args:t})=>{if(!t[0])return e.reply(`Que desea descargar de Youtube?, Ejemplo de uso: \n\n${Prefijo+command} https://youtu.be/ed-6VSF-GGc`);if(!isUrl(t[0])&&!t[0].includes("youtu"))return reply("*[ ! ] Link inválido*\n_Por favor, use un link de YouTube_\n");const i=await a.getName(e.sender),o=e.reply(MultiNK.Proces(i));await o;try{const i=(t[1]||"360")+"p",o=t[0],r=await youtubedl(o).catch((async()=>await youtubedlv2(o))).catch((async()=>await youtubedlv3(o))),n=await r.video[i].download(),l=await r.title,u=await r.video[i].fileSizeH;if(u.split("MB")[0]>=50)return e.reply("[ ! ] Tu wea pesa demasiado, sorry mi king no podre enviarlo :v");await a.sendMessage(e.chat,{video:{url:n},fileName:`${l}.mp4`,mimetype:"video/mp4",caption:`Titulo: ${l}\nTamaño: ${u}\nExtencion: .mp4`},{quoted:e})}catch{await a.reply(e.chat,MultiNK.Error1(),e)}};handler.help=["ytvbochi <link>"],handler.tags=["servicio"],handler.command=/^ytvbochi$/i;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
