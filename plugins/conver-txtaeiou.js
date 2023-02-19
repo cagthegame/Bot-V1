36d7f4735cdd20b9cffc267c8f660e7838f7e3df
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 02:34:24 +0000

    @NeKosmic 🙀

--- plugins/conver-txtaeiou.js
+++ plugins/conver-txtaeiou.js
@@ -3,10 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(e,{conn:t,command:a,text:o})=>{if(!e.quoted&&!o)return e.reply(`Envie o responda un texto, ejemplo de uso:
-
-${Prefijo+a} lenguaje inclusivo
-`);try{var r,n=a[1].toLowerCase();await t.sendMessage(e.chat,{text:(e.quoted?e.quoted.text:o).replace(/[aiueo]/g,n).replace(/[AIUEO]/g,n.toUpperCase())},{quoted:e}),reacMoji(e.chat,t,"\uD83D\uDC85",e)}catch(l){e.reply(MultiNK.Error1())}};handler.help=["taxta | texte | tixti | toxto | tuxtu"],handler.tags=["conversor"],handler.command=/^(taxta|texte|tixti|toxto|tuxtu)$/i;export default handler;
+let handler=async(e,{conn:t,command:a,text:o})=>{if(!e.quoted&&!o)return e.reply(`Envie o responda un texto, ejemplo de uso:\n\n${Prefijo+a} lenguaje inclusivo\n`);try{const r=a[1].toLowerCase(),n=e.quoted?e.quoted.text:o;await t.sendMessage(e.chat,{text:n.replace(/[aiueo]/g,r).replace(/[AIUEO]/g,r.toUpperCase())},{quoted:e}),reacMoji(e.chat,t,"💅",e)}catch(t){e.reply(MultiNK.Error1())}};handler.help=["taxta | texte | tixti | toxto | tuxtu"],handler.tags=["conversor"],handler.command=/^(taxta|texte|tixti|toxto|tuxtu)$/i;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
