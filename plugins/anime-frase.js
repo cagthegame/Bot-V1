18c56ffacc1ceb1fb68c1ac7ab7b8982a305cd12
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 03:24:44 +0000

    @NeKosmic 🙀

--- plugins/anime-frase.js
+++ plugins/anime-frase.js
@@ -3,11 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-let handler=async(e,{conn:a})=>{let t=await a.profilePictureUrl(e.sender,"image").catch(e=>"./multimedia/imagenes/avatar_contact.png");try{var n=await fetchJson(`https://latam-api.vercel.app/api/ani_frases?apikey=${MyApiKey}`);a.relayMessage(e.chat,{extendedTextMessage:{text:`
-🎴 Anime : ${n.anime}
-🀄 Personaje : ${n.personaje}
-🔖 Frase : ${n.frase}
-`,contextInfo:{mentionedJid:[e.sender],externalAdReply:{thumbnailUrl:t,sourceUrl:"https://vm.tiktok.com/ZMF1heqbV/"}}}},{})}catch(r){e.reply(MultiNK.Error0())}};handler.help=["anifrase"],handler.tags=["animeuwu"],handler.command=/^(anifrase)$/i,handler.limit=!0;export default handler;
+let handler=async(e,{conn:a})=>{const n=await a.profilePictureUrl(e.sender,"image").catch((e=>"./multimedia/imagenes/avatar_contact.png"));try{const t=await fetchJson(`https://latam-api.vercel.app/api/ani_frases?apikey=${MyApiKey}`);a.relayMessage(e.chat,{extendedTextMessage:{text:`\n🎴 Anime : ${t.anime}\n🀄 Personaje : ${t.personaje}\n🔖 Frase : ${t.frase}\n`,contextInfo:{mentionedJid:[e.sender],externalAdReply:{thumbnailUrl:n,sourceUrl:"https://vm.tiktok.com/ZMF1heqbV/"}}}},{})}catch(a){e.reply(MultiNK.Error0())}};handler.help=["anifrase"],handler.tags=["animeuwu"],handler.command=/^anifrase$/i,handler.limit=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
