e3665074d8c76ab8634e0ad11b830b72bb1ecfe8
Matt Meléndez <tslrame369@gmail.com>
2023-02-19 02:13:02 +0000

    @NeKosmic 🙀

--- plugins/dl-yta.js
+++ plugins/dl-yta.js
@@ -3,9 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import*as e from"fs";let handler=async(a,{conn:t,command:r,text:l,args:i})=>{if(!l)return a.reply(`Que desea descargar de Youtube?, Ejemplo de uso: 
-
-${Prefijo+r} https://youtu.be/vbhVFqYwxLM`);if(!isUrl(i[0])&&!i[0].includes("youtu"))return reply("*[ ! ] Link inv\xe1lido*\n_Por favor, use un link de YouTube_\n");let n;await a.reply(MultiNK.Proces(await t.getName(a.sender)));let o=encodeURIComponent(l);try{let d=await fetchJson(`https://latam-api.vercel.app/api/ytmp3?apikey=${MyApiKey}&q=${o}`);await getBuffer(d.logo),await t.sendMessage(a.chat,{document:{url:d.descarga},mimetype:"audio/mpeg",fileName:`${d.titulo}.mp3`,jpegThumbnail:e.readFileSync("./multimedia/imagenes/logo.jpg")},{quoted:a}).catch(e=>{a.reply(MultiNK.Error1())})}catch(u){a.reply(MultiNK.Error0())}};handler.help=["yta <link>"],handler.tags=["servicio"],handler.command=/^(yta)$/i,handler.limit=!0;export default handler;
+import fs from"fs";let handler=async(e,{conn:a,command:t,text:r,args:i})=>{if(!r)return e.reply(`Que desea descargar de Youtube?, Ejemplo de uso: \n\n${Prefijo+t} https://youtu.be/vbhVFqYwxLM`);if(!isUrl(i[0])&&!i[0].includes("youtu"))return reply("*[ ! ] Link inválido*\n_Por favor, use un link de YouTube_\n");const n=await a.getName(e.sender),o=e.reply(MultiNK.Proces(n));await o;const l=encodeURIComponent(r);try{const t=await fetchJson(`https://latam-api.vercel.app/api/ytmp3?apikey=${MyApiKey}&q=${l}`);await getBuffer(t.logo);await a.sendMessage(e.chat,{document:{url:t.descarga},mimetype:"audio/mpeg",fileName:`${t.titulo}.mp3`,jpegThumbnail:fs.readFileSync("./multimedia/imagenes/logo.jpg")},{quoted:e}).catch((a=>{e.reply(MultiNK.Error1())}))}catch(a){e.reply(MultiNK.Error0())}};handler.help=["yta <link>"],handler.tags=["servicio"],handler.command=/^yta$/i,handler.limit=!0;export default handler;
 
 /**
 [_>] https://github.com/NeKosmic/
